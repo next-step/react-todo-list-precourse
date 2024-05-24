80766f649df6358503eec0e6d578a66ab0161136
tteokbokki-master <0jin.git@gmail.com>
2024-05-24 17:16:40 +0900

    Feat: 오늘 날짜 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
 import "./styles/Header.css";
+
+const date = new Date();
+const dayArray = ["일", "월", "화", "수", "목", "금", "토"];
+const currentDay = date.getDay();
+
 const Header = () => {
   return (
     <div className="Header">
-      <p>투두 리스트</p>
-      <p>24년 5월 24일</p>
+      <p>TodoList</p>
+      <p>
+        {`${date.getFullYear()}년 ${
+          date.getMonth() + 1
+        }월 ${date.getDate()}일 ${dayArray[currentDay]}요일`}
+      </p>
     </div>
   );
 };
