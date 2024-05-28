9fabd036138b51d7803feae60f07a3e636c362f4
cla6shade <cla6shade@gmail.com>
2024-05-28 14:54:27 +0900

    feat: add import statement for main.css

--- src/components/main/MainContainer.tsx
+++ src/components/main/MainContainer.tsx
@@ -1,6 +1,8 @@
 import MainHeader from './MainHeader';
 import MainFooter from './MainFooter';
 
+import '../../css/main.css'
+
 const MainContainer = () => {
   return <div className='main-container'>
     <MainHeader title={'TODOS'} />
