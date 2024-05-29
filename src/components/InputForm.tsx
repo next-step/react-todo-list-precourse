29610f24b29e88968ce57fdef89c150ccda8df52
박건규 <geongyu09@gmail.com>
2024-05-29 14:21:51 +0900

    feat: 사용자의 input을 받는 컴포넌트를 추가합니다 (InputForm.tsx)
    사용자의 input을 받는 컴포넌트를 추가합니다.
    - input의 값을 가져올 수 있도록 state를 추가합니다.
    - submit 을 받을 수 있도록 이벤트를 추가합니다.

---
+++ src/components/InputForm.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { Todo } from "../Modules/Todo";
+
+interface InputFormProps {
+  setTodos: (newTodo: Todo) => void;
+}
+
+const InputForm = ({ setTodos }: InputFormProps) => {
+  const [text, setText] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const newTodo: Todo = { text, status: "active" };
+    setTodos(newTodo);
+    setText("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="What need to be done?"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+    </form>
+  );
+};
+
+export default InputForm;
