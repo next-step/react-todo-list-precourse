1ccb8bf07c331675bf887ea63dfb6b2fe0475815
cla6shade <cla6shade@github.com>
2024-05-30 16:35:42 +0900

    feat: add callback for enter key input

--- src/components/todo/TodoInput.tsx
+++ src/components/todo/TodoInput.tsx
@@ -1,6 +1,16 @@
+import {useCallback, useRef} from 'react';
+
 type TodoInputProps = {
   addTodo: (content: string) => void;
 };
 const TodoInput = ({ addTodo }: TodoInputProps) => {
-  return <></>
+  const inputRef = useRef<HTMLInputElement>(null);
+  const onKeyDown = useCallback((e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key !== 'Enter' || ! inputRef.current) return;
+    const content = inputRef.current.value;
+    addTodo(content);
+  }, [addTodo]);
+  return <div>
+    <input ref={inputRef} onKeyDown={onKeyDown} />
+  </div>
 };
