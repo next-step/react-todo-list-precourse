1e629162254e8a5fd15bb37483ba3b15d162e296
cla6shade <cla6shade@gmail.com>
2024-05-30 21:38:15 +0900

    feat: add content and icon to TodoItem

--- src/components/todo/TodoItem.tsx
+++ src/components/todo/TodoItem.tsx
@@ -1,9 +1,20 @@
 import Todo from '../../models/Todo.ts';
+import icon_close from '../../assets/icon_close.svg';
 
 type TodoItemProps = {
   todo: Todo,
   removeTodo: (id: number) => void,
   handleToggleCompletion: (todo: Todo) => void;
+};
+
+type TodoContentTextProps = {
+  content: string
+};
+
+const TodoContentText = ({ content }: TodoContentTextProps) => {
+  return <div>
+    <p>{ content }</p>
+  </div>
 };
 const TodoItem = ({ todo, removeTodo, handleToggleCompletion }: TodoItemProps) => {
   const onCheckChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -11,7 +22,8 @@ const TodoItem = ({ todo, removeTodo, handleToggleCompletion }: TodoItemProps) =
   };
   return <div>
     <input type='checkbox' defaultChecked={todo.isCompleted} onChange={onCheckChange}/>
-    { todo.content }
+    <TodoContentText content={todo.content} />
+    <img src={icon_close} />
   </div>
 };
 export default TodoItem;
