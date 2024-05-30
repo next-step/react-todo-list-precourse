a17aa04e6380d961e4ee44f0b755fc57927a22b3
cla6shade <cla6shade@github.com>
2024-05-30 17:12:32 +0900

    feat: add filter button component

--- src/components/todo/TodoButtonBar.tsx
+++ src/components/todo/TodoButtonBar.tsx
@@ -5,13 +5,18 @@ type TodoButtonBarProps = {
   setFilter: (filter: TodoFilterEnum) => void,
   removeCompleteTodos: () => void
 };
+type TodoFilterButtonProps = {
+  setFilter: (filter: TodoFilterEnum) => void,
+  filterTarget: TodoFilterEnum
+}
+const TodoFilterButton = ({ setFilter, filterTarget }: TodoFilterButtonProps) => {
+  return <button onClick={() => setFilter(filterTarget)}>
+    { filterTarget }
+  </button>
+}
 const TodoButtonBar = ({ activeCount, setFilter }: TodoButtonBarProps) => {
   return <div>
     <p>{ activeCount } items left!</p>
-    <button>All</button>
-    <button>Active</button>
-    <button>Completed</button>
-    <button>Clear Completed</button>
   </div>
 };
 export default TodoButtonBar;
