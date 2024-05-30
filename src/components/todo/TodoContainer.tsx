25a111897c8020c53c94a186c1906ed8c1a142be
cla6shade <cla6shade@github.com>
2024-05-30 15:15:56 +0900

    feat: add todos state for todo repository
    todos는 id=>Todo 형태로 todo를 저장하는 state임.

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,4 +1,12 @@
+import {useState} from 'react';
+import Todo from '../../models/Todo.ts';
+
+type TodoRepository = {
+  [id: number]: Todo;
+};
+
 const TodoContainer = () => {
+  const [todos, setTodos] = useState<TodoRepository>({});
   return <></>;
 };
 export default TodoContainer;
