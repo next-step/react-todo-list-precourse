a3473f72ac81ddb46ddaf7faf4026d10371893c9
cla6shade <cla6shade@github.com>
2024-05-30 18:20:00 +0900

    feat: make todobox to print todos with TodoItem component

--- src/components/todo/TodoBox.tsx
+++ src/components/todo/TodoBox.tsx
@@ -1,6 +1,7 @@
 import Todo from '../../models/Todo.ts';
 import TodoFilterEnum from '../../constants/TodoFilterEnum.ts';
 import {useCallback} from 'react';
+import TodoItem from './TodoItem.tsx';
 
 type TodoRepository = {
   [id: number]: Todo;
@@ -25,5 +26,9 @@ const TodoBox = ({ todos, filter, removeTodo, handleToggleCompletion }: TodoBoxP
     if(filter === TodoFilterEnum.TODO_FILTER_ALL) return getAllTodos();
     return filter === TodoFilterEnum.TODO_FILTER_ACTIVE ? getActiveTodos() : getCompletedTodos();
   }, [todos, filter]);
+  return getFilteredTodos().map((todo: Todo) => {
+    const key = 'todo-id-' + todo.id;
+    return <TodoItem todo={todo} key={key} removeTodo={removeTodo} handleToggleCompletion={handleToggleCompletion}/>;
+  });
 };
 export default TodoBox;
