9720c74bf0b97cf98728c59347268975cea6637b
cla6shade <cla6shade@github.com>
2024-05-30 15:49:45 +0900

    feat: set MainContainer to render new child: TodoContainer

--- src/components/main/MainContainer.tsx
+++ src/components/main/MainContainer.tsx
@@ -2,10 +2,12 @@ import MainHeader from './MainHeader';
 import MainFooter from './MainFooter';
 
 import '../../css/main.css'
+import TodoContainer from '../todo/TodoContainer.tsx';
 
 const MainContainer = () => {
   return <div className='main-container'>
     <MainHeader title={'TODOS'} />
+    <TodoContainer />
     <MainFooter />
   </div>;
 };
