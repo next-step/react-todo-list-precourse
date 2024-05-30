fcc683fc994b5cc92daa4ab0af20ea1156c08f09
cla6shade <cla6shade@github.com>
2024-05-30 17:51:28 +0900

    style: add styles and layout for buttons

--- src/components/todo/TodoButtonBar.tsx
+++ src/components/todo/TodoButtonBar.tsx
@@ -1,5 +1,7 @@
 import TodoFilterEnum from '../../constants/TodoFilterEnum';
 
+import styles from '../../css/todo.module.css';
+
 type TodoButtonBarProps = {
   activeCount: number,
   setFilter: (filter: TodoFilterEnum) => void,
@@ -13,22 +15,22 @@ type TodoButtonContainerProps = {
   setFilter: (filter: TodoFilterEnum) => void
 }
 const TodoButtonContainer = ({ setFilter }: TodoButtonContainerProps) => {
-  return <div>
+  return <div className={styles.todoBtnContainer}>
     <TodoFilterButton setFilter={setFilter} filterTarget={TodoFilterEnum.TODO_FILTER_ALL} />
     <TodoFilterButton setFilter={setFilter} filterTarget={TodoFilterEnum.TODO_FILTER_ACTIVE} />
     <TodoFilterButton setFilter={setFilter} filterTarget={TodoFilterEnum.TODO_FILTER_COMPLETED} />
   </div>
 }
 const TodoFilterButton = ({ setFilter, filterTarget }: TodoFilterButtonProps) => {
-  return <button onClick={() => setFilter(filterTarget)}>
+  return <button onClick={() => setFilter(filterTarget)} className={styles.todoBtn}>
     { filterTarget }
   </button>
 }
 const TodoButtonBar = ({ activeCount, setFilter, removeCompleteTodos }: TodoButtonBarProps) => {
-  return <div>
+  return <div className={styles.todoBtnBar}>
     <p>{ activeCount } items left!</p>
     <TodoButtonContainer setFilter={setFilter} />
-    <p onClick={removeCompleteTodos}>Clear completed</p>
+    <p onClick={removeCompleteTodos} className={styles.clearText}>Clear completed</p>
   </div>
 };
 export default TodoButtonBar;
