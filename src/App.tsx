92c556d934968619ff0c0d5eba6a1807ddc08cf5
ajin <aj5210@naver.com>
2024-05-30 23:33:50 +0900

    Feat(App): 초기 HTML 구조 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,48 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div>test</div>;
+  const [todos, setTodos] = useState<string[] | null>();
+
+  useEffect(() => {
+    setTodos(["테스트1", "테스트2", "테스트3"]);
+  }, []);
+
+  return (
+    <div className="container">
+      <header>
+        <h1>To Do List</h1>
+        <div className="input block">
+          <input placeholder="할 일을 입력해주세요." />
+          <button>추가</button>
+        </div>
+      </header>
+      <section>
+        <div className="todo-list">
+          {todos?.map((todo) => {
+            return (
+              <div className="todo-item block">
+                <input type="checkbox" />
+                <label>{todo}</label>
+                <button>제거</button>
+              </div>
+            );
+          })}
+        </div>
+        <nav className="block">
+          <span className="nav-item">0 개</span>
+          <div className="nav-item">
+            <button>모두</button>
+            <button>진행중</button>
+            <button>완료</button>
+          </div>
+          <div className="nav-item">
+            <button>완료된 할 일 제거하기</button>
+          </div>
+        </nav>
+      </section>
+    </div>
+  );
 }
 
 export default App;
