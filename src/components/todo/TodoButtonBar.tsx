a3401b8ab92ed9a156e2b4a66f7caa0fe10ddf58
cla6shade <cla6shade@github.com>
2024-05-30 17:46:06 +0900

    feat: add todo button container component

--- src/components/todo/TodoButtonBar.tsx
+++ src/components/todo/TodoButtonBar.tsx
@@ -9,6 +9,16 @@ type TodoFilterButtonProps = {
   setFilter: (filter: TodoFilterEnum) => void,
   filterTarget: TodoFilterEnum
 }
+type TodoButtonContainerProps = {
+  setFilter: (filter: TodoFilterEnum) => void
+}
+const TodoButtonContainer = ({ setFilter }: TodoButtonContainerProps) => {
+  return <div>
+    <TodoFilterButton setFilter={setFilter} filterTarget={TodoFilterEnum.TODO_FILTER_ALL} />
+    <TodoFilterButton setFilter={setFilter} filterTarget={TodoFilterEnum.TODO_FILTER_ACTIVE} />
+    <TodoFilterButton setFilter={setFilter} filterTarget={TodoFilterEnum.TODO_FILTER_COMPLETED} />
+  </div>
+}
 const TodoFilterButton = ({ setFilter, filterTarget }: TodoFilterButtonProps) => {
   return <button onClick={() => setFilter(filterTarget)}>
     { filterTarget }
@@ -17,9 +27,7 @@ const TodoFilterButton = ({ setFilter, filterTarget }: TodoFilterButtonProps) =>
 const TodoButtonBar = ({ activeCount, setFilter, removeCompleteTodos }: TodoButtonBarProps) => {
   return <div>
     <p>{ activeCount } items left!</p>
-    <TodoFilterButton setFilter={setFilter} filterTarget={TodoFilterEnum.TODO_FILTER_ALL} />
-    <TodoFilterButton setFilter={setFilter} filterTarget={TodoFilterEnum.TODO_FILTER_ACTIVE} />
-    <TodoFilterButton setFilter={setFilter} filterTarget={TodoFilterEnum.TODO_FILTER_COMPLETED} />
+    <TodoButtonContainer setFilter={setFilter} />
     <p onClick={removeCompleteTodos}>Clear completed</p>
   </div>
 };
