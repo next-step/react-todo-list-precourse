c29030f04b1e7ff05a8d90b774524f147b82de7b
cla6shade <cla6shade@github.com>
2024-05-30 17:07:26 +0900

    feat: add text indicator for Todo Filter
    필터명을 출력할 때 사용할 수 있도록 Enum 수정

--- src/constants/TodoFilterEnum.ts
+++ src/constants/TodoFilterEnum.ts
@@ -1,6 +1,6 @@
 enum TodoFilterEnum {
-  TODO_FILTER_ALL,
-  TODO_FILTER_ACTIVE,
-  TODO_FILTER_COMPLETED
+  TODO_FILTER_ALL = "All",
+  TODO_FILTER_ACTIVE = "Active",
+  TODO_FILTER_COMPLETED = "Completed"
 }
 export default TodoFilterEnum;
