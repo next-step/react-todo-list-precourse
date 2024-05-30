c5832e7711219039bf7761182674915223d8e056
cla6shade <cla6shade@gmail.com>
2024-05-30 21:53:08 +0900

    feat: add click handler to close button

--- src/components/todo/TodoItem.tsx
+++ src/components/todo/TodoItem.tsx
@@ -24,7 +24,7 @@ const TodoItem = ({ todo, removeTodo, handleToggleCompletion }: TodoItemProps) =
   return <div>
     <input type='checkbox' defaultChecked={todo.isCompleted} onChange={onCheckChange}/>
     <TodoContentText content={todo.content} />
-    <img src={icon_close} />
+    <img src={icon_close} onClick={() => removeTodo(todo.id)}/>
   </div>
 };
 export default TodoItem;
