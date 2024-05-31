d044fe7804d3e37cb4fc0754aff6b0c8376772ff
JaeAnHan <jaean1999@naver.com>
2024-06-01 00:06:21 +0900

    feat: replace input event handler
    to accept enter key code

--- src/components/todos/TodoInput.jsx
+++ src/components/todos/TodoInput.jsx
@@ -1,13 +1,21 @@
 import { Fragment, useState } from "react";
 
-export function TodoInput() {
-    const [value, setValue] = useState("");
-    const handleInputChange = (e) => setValue(e.target.value);
+const handleKeyDown = (e, addTodo, setValue) => {
+    if (e.keyCode === 13) {
+        addTodo(e.target.value);
+        e.target.value = "";
+        setValue("");
+        return;
+    }
+    setValue(e.target.value);
+}
 
+export function TodoInput({ addTodo }) {
+    const [value, setValue] = useState("");
     return (
         <Fragment>
             <label htmlFor={"input_id"}>{value}</label>
-            <input id={"input_id"} onChange={handleInputChange}/>
+            <input id={"input_id"} onKeyDown={(e) => handleKeyDown(e, addTodo, setValue)}/>
         </Fragment>
     );
 }
