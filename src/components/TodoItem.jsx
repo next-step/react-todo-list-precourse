4b82674dfb251af935cff5432ef54985ce457c0e
loveydev <tjgus9650@gmail.com>
2024-05-31 22:56:06 +0900

    Feat: Add toggle box

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,20 +1,22 @@
-import React from "react";
+import React from 'react'
 
 const TodoItem = ({ task, toggleComplete, deleteTodo, editTodo }) => {
   return (
-    <div className="TodoItem">
-      <input type="checkbox" className="toggle-box" onClick={() => toggleComplete(task.id)} />
+    <div className='TodoItem'>
+      <div className={task.completed ? 'checked' : 'unchecked'} onClick={() => toggleComplete(task.id)}></div>
 
-      <p className={`${task.completed ? "completed" : ""}`} >
-        {task.task}
-      </p>
+      <p className={`${task.completed ? 'completed' : ''}`}>{task.task}</p>
 
-      <div className="set-btn">
-        <button className="edit-btn" onClick={() => editTodo(task.id)}>Edit</button>
-        <button className="delete-btn" onClick={() => deleteTodo(task.id)}>Delete</button>
+      <div className='set-btn'>
+        <button className='edit-btn' onClick={() => editTodo(task.id)}>
+          Edit
+        </button>
+        <button className='delete-btn' onClick={() => deleteTodo(task.id)}>
+          Delete
+        </button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default TodoItem;
+export default TodoItem
