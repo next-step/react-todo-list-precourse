d05a14d38203ba26784769d6164e87a3f27b416a
loveydev <tjgus9650@gmail.com>
2024-06-01 00:09:01 +0900

    Feat: Add Select All button

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -3,7 +3,19 @@ import TodoForm from './TodoForm'
 import TodoItem from './TodoItem'
 import EditTodoForm from './EditTodoForm'
 import Filter from './Filter'
-import { addTodo, deleteTodo, toggleComplete, editTodo, editTask } from './Actions'
+import SelectAll from './SelectAll'
+import DeleteCompleteTodo from './DeleteCompleteTodo'
+import {
+  addTodo,
+  deleteTodo,
+  toggleComplete,
+  editTodo,
+  editTask,
+  completeAllTodos,
+  incompleteAllTodos,
+  deleteCompletedTodos,
+  getIncompleteCount,
+} from './Actions'
 
 const TodoContainer = () => {
   const [todos, setTodos] = useState([])
@@ -16,7 +28,7 @@ const TodoContainer = () => {
 
     if (active === 1) filteredTodos = todos.filter(todo => !todo.completed)
     else if (active === 2) filteredTodos = todos.filter(todo => todo.completed)
-  
+
     return filteredTodos.map((todo, index) =>
       todo.isEditing ? (
         <EditTodoForm editTodo={(task, id) => editTask(task, id, todos, setTodos)} task={todo} key={index} />
@@ -38,6 +50,8 @@ const TodoContainer = () => {
       <TodoForm addTodo={todo => addTodo(todo, todos, setTodos, nextId, setNextId)} />
       <Filter active={active} setActive={setActive} />
       {renderTodoItems(todos, editTask, toggleComplete, deleteTodo, editTodo)}
+      <SelectAll allCheck={completeAllTodos} allUncheck={incompleteAllTodos} todos={todos} setTodos={setTodos} />
+      <DeleteCompleteTodo clearTodos={deleteCompletedTodos} todos={todos} setTodos={setTodos} remainCount={getIncompleteCount} />
     </div>
   )
 }
