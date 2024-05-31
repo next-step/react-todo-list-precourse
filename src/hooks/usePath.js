a945c8bce3f874117ba3a86b850afb98716b263f
JaeAnHan <jaean1999@naver.com>
2024-06-01 01:17:01 +0900

    feat: apply filtering todos on history back and forth
    TODO: fix two effect hooks on same dependency

--- src/hooks/usePath.js
+++ src/hooks/usePath.js
@@ -1,10 +1,20 @@
 import { useCallback, useEffect, useState } from "react";
 import { PATH_ALL } from "../constants.js";
 
+const handlePopState = (path, setPath) => {
+    if (path !== document.location.hash) {
+        setPath(document.location.hash)
+    }
+}
+
 export function usePath() {
     const [pathName, setPathName] = useState(PATH_ALL);
     const updatePathName = useCallback((path) => setPathName(path), []);
     useEffect(() => window.history.pushState(null, "", pathName), [pathName]);
+    useEffect(() => { // TODO: 합치면 url hash가 안바뀌는데 왜지
+        window.addEventListener('popstate', () => handlePopState(pathName, setPathName));
+        return () => window.removeEventListener('popstate', () => handlePopState(pathName, setPathName));
+    }, []);
     console.log('path');
 
     return { path : pathName, setPath: updatePathName };
