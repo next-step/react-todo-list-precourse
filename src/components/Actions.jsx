85c6a1c8fa38c9f8a46baafc93fefa6e6505c62c
loveydev <tjgus9650@gmail.com>
2024-06-01 00:08:33 +0900

    Feat: Add select function

--- src/components/Actions.jsx
+++ src/components/Actions.jsx
@@ -1,5 +1,5 @@
-import EditTodoForm from "./EditTodoForm"
-import TodoItem from "./TodoItem"
+import EditTodoForm from './EditTodoForm'
+import TodoItem from './TodoItem'
 
 // 할 일을 추가하는 함수
 export const addTodo = (todo, todos, setTodos, nextId, setNextId) => {
@@ -26,4 +26,24 @@ export const editTodo = (id, todos, setTodos) => {
 export const editTask = (task, id, todos, setTodos) => {
   setTodos(todos.map(todo => (todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo)))
 }
+
+// 전체 할 일을 완료 상태로 변경하는 함수
+export const completeAllTodos = (todos, setTodos) => {
+  setTodos(todos.map(todo => ({ ...todo, completed: true })))
+}
+
+// 전체 할 일을 미완료 상태로 변경하는 함수
+export const incompleteAllTodos = (todos, setTodos) => {
+  setTodos(todos.map(todo => ({ ...todo, completed: false })))
+}
+
+// 완료된 할 일을 일괄 삭제하는 함수
+export const deleteCompletedTodos = (todos, setTodos) => {
+  setTodos(todos.filter(todo => !todo.completed))
+}
+
+// 완료되지 않은 할 일의 개수를 계산하는 함수
+export const getIncompleteCount = (todos) => {
+  return todos.filter(todo => !todo.completed).length;
+};
 
