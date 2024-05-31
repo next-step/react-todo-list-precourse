ef4a24902aba4ce88f133992c722623e341ac152
JaeAnHan <jaean1999@naver.com>
2024-06-01 00:13:39 +0900

    feat: update TodoList Component using hooks
    hooks : usePath, useTodoList

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,22 +1,24 @@
-import { useCallback, useEffect, useState } from "react";
 import { PATH_ALL } from "../constants.js";
 import TodoHeader from "./todos/TodoHeader.jsx";
 import { TodoInput } from "./todos/TodoInput.jsx";
+import { useTodoList } from "../hooks/useTodoList.js";
+import { usePath } from "../hooks/usePath.js";
 
 export const createFilterName = (path) => {
-    if (path === PATH_ALL) return "All";
+    if (path === PATH_ALL) {
+        return "All";
+    }
     return path.charAt(0).toUpperCase() + path.slice(3);
 }
 
 export function TodoList() {
-    const [pathName, setPathName] = useState(PATH_ALL);
-    const updatePathName = useCallback((path) => setPathName(path), []);
-    useEffect(() => window.history.pushState(null, "", pathName), [pathName]);
-    console.log('todolist', createFilterName(pathName));
+    const { path, setPath } = usePath();
+    console.log('todolist', createFilterName(path));
+    const { todos, addTodo, updateTodoState, deleteTodo } = useTodoList();
     return (
         <div>
-            <TodoHeader updatePathName={updatePathName} />
-            <TodoInput />
+            <TodoHeader updatePathName={setPath} />
+            <TodoInput addTodo={addTodo} />
         </div>
     );
 }
