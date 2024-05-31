8809113d19dc36921a5e5ed7cc39b68756563bd5
seung365 <bdh3659@naver.com>
2024-05-31 12:47:56 +0900

    refactor: App.js useCallback 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import React, { useState, useCallback, useRef } from "react"
+import React, { useState, useRef } from "react"
 import "./App.css"
 import TodoInsert from "./components/TodoInsert"
-
 import List from "./components/List"
 import Active from "./components/Active"
 import Completed from "./components/Completed"
@@ -11,32 +10,25 @@ function App() {
   const [todos, setTodos] = useState([])
   const navigate = useNavigate()
   const nextId = useRef(1)
+  const onInsert = (text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    }
+    if (text !== "") {
+      setTodos(todos.concat(todo))
+      nextId.current += 1
+    }
+  }
 
-  const onInsert = useCallback(
-    (text) => {
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      }
-      if (text !== "") {
-        setTodos(todos.concat(todo))
-        nextId.current += 1
-      }
-      // 인자로 주어진 배열이나 값들을 기존 배열과 합쳐 새배열 생성
-    },
-    [todos]
-  )
-  const onRemove = useCallback(
-    (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id))
-    },
-    [todos]
-  )
+  const onRemove = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id))
+  }
 
-  const onTouched = useCallback((id) => {
+  const onTouched = (id) => {
     setTodos(todos.map((todo) => (todo.id === id ? { ...todo, checked: !todo.checked } : todo)))
-  })
+  }
 
   const itemLeft = todos.filter((todo) => todo.checked === false)
 
@@ -53,14 +45,12 @@ function App() {
             element={<Completed todos={todos} onRemove={onRemove} onTouched={onTouched} />}
           ></Route>
         </Routes>
-
         <button onClick={() => navigate("/")}> All </button>
         <button onClick={() => navigate("/active")}> Active </button>
         <button onClick={() => navigate("/completed")}> Completed </button>
       </div>
-      <footer>{`${itemLeft.length}items left!`}</footer>
+      <footer>{`${itemLeft.length} items left!`}</footer>
     </div>
   )
 }
-
 export default App
