c65658a240e3cdba891c5ef51098d02ac5f55350
loveydev <tjgus9650@gmail.com>
2024-05-31 23:02:19 +0900

    Refactor: remove parenthesis

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import TodoItem from "./TodoItem";
+import React from 'react'
+import TodoItem from './TodoItem'
 
 const TodoList = ({ todos }) => {
   return (
     <div>
-      {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo}/>
+      {todos.map(todo => (
+        <TodoItem key={todo.id} todo={todo} />
       ))}
     </div>
-  );
-};
+  )
+}
 
-export default TodoList;
+export default TodoList
