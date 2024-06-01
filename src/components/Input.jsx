7431287d39893b69814b8752cc7cc53aee04fcba
minji2219 <200146@jnu.ac.kr>
2024-06-01 22:12:50 +0900

    Feat: 엔터 동작 구현
    일정 입력 후 엔터 입력시 todos 상태변화

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,17 +1,25 @@
 import { useContext, useState } from "react";
 import "../styles/Input.css";
-// import { TodosContext } from "../context/todosContext";
+import { TodosContext } from "../context/TodosContext";
 
 const Input = () => {
   const [todoText, setTodoText] = useState("");
+  const { todos, setTodos } = useContext(TodosContext);
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      setTodos([{ todo: todoText, checked: false }, ...todos]);
+      setTodoText("");
+    }
+  };
   return (
     <div>
       <input
         className="inputbox"
         type="text"
         placeholder="일정을 입력하세요."
+        value={todoText}
         onChange={(e) => setTodoText(e.target.value)}
-        // onKeyDown={handleKeyDown}
+        onKeyDown={handleKeyDown}
       />
     </div>
   );
