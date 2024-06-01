1f5e5872a276c1c5ea82a2bd7766f8371b29fb48
harugi7 <gimme2721@gmail.com>
2024-06-01 13:37:38 +0900

    connect to dom file

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './App.css';
+import Main from './Main';
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(React.createElement(Main));
