5ae9758a0d52300b824df0c1cb299630003b6b47
JaeAnHan <jaean1999@naver.com>
2024-06-01 11:39:36 +0900

    fix: change dependency on event handling effect hook(usePath)

--- src/hooks/usePath.js
+++ src/hooks/usePath.js
@@ -10,12 +10,12 @@ const handlePopState = (path, setPath) => {
 export function usePath() {
     const [pathName, setPathName] = useState(PATH_ALL);
     const updatePathName = useCallback((path) => setPathName(path), []);
+    // TODO: 복사본 따로 저장 안하고 여러번하려면 어떻게 해야하지
     useEffect(() => window.history.pushState(null, "", pathName), [pathName]);
     useEffect(() => { // TODO: 합치면 url hash가 안바뀌는데 왜지
         window.addEventListener('popstate', () => handlePopState(pathName, setPathName));
         return () => window.removeEventListener('popstate', () => handlePopState(pathName, setPathName));
-    }, []);
-    console.log('path');
+    }, [pathName]);
 
     return { path : pathName, setPath: updatePathName };
 }
