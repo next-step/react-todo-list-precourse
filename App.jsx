b913f9fd1f5684752fcf569b6ad99328dbe6bf89
jihoon7171 <jihoon7171@naver.com>
2024-06-01 23:40:13 +0900

    [Add]일기 list,thing 모델링

--- App.jsx
+++ App.jsx
@@ -1,8 +1,22 @@
+import React, {useState} from "react"
 import "./App.css"
 import Header from "./src/component/Header"
 import List from "./src/component/List"
 import Maker from "./src/component/Maker"
 function App(){
+  const tododata = [
+    {id :0,
+    isDone : false, 
+    content : "asdf"},
+    {id :1,
+      isDone : false, 
+      content : "asdfasdf"},
+    {id :2,
+      isDone : false, 
+      content : "asdfasdfasdf"}
+    
+  ]
+  const [TodoList, setTodoList] = useState(tododata)
     return (
      <div className="App">
    <Header/>
