90f80ed85389ef7297ee979b5aafe444b5f7f197
JaeAnHan <jaean1999@naver.com>
2024-06-01 18:04:20 +0900

    feet: add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Header } from "./components/Header.jsx";
 import { TodoList } from "./components/TodoList.jsx";
+import { Footer } from "./components/Footer.jsx";
 
 export function App() {
     console.log('app')
@@ -7,6 +8,7 @@ export function App() {
         <div className="App">
             <Header />
             <TodoList />
+            <Footer />
         </div>
     );
 }
