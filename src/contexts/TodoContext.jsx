20d332b67dc8e18a9f1f880a095861b8ea018484
tteokbokki-master <0jin.git@gmail.com>
2024-06-02 03:13:42 +0900

    Feat: localStorage로 데이터 전달 및 받아오기 구현

--- src/contexts/TodoContext.jsx
+++ src/contexts/TodoContext.jsx
@@ -1,9 +1,14 @@
-import { useState, createContext } from "react";
+import { useState, useEffect, createContext } from "react";
+
+let initDate = window.localStorage.getItem("todos") ? JSON.parse(window.localStorage.getItem("todos")) : [];
 
 export const TodoContext = createContext();
 
 export const TodoProvider = ({ children }) => {
-  const [todoItems, setTodoItems] = useState([]);
+  const [todoItems, setTodoItems] = useState(initDate);
+  useEffect(() => {
+    window.localStorage.setItem("todos", JSON.stringify(todoItems));
+  }, [todoItems]);
 
   return <TodoContext.Provider value={{ todoItems, setTodoItems }}>{children}</TodoContext.Provider>;
 };
