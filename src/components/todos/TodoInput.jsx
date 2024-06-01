63c6e10898c6aad3d71ba979e6cf016116c0ec01
JaeAnHan <jaean1999@naver.com>
2024-06-02 03:02:30 +0900

    feat(TodoInput): change event listening KeyDown -> Change
    - due to setState delay on KeyDownHandler
    - <div>{state}</div> not equal to the latest input

--- src/components/todos/TodoInput.jsx
+++ src/components/todos/TodoInput.jsx
@@ -1,13 +1,7 @@
 import { useRef, useState } from "react";
 
-const handleKeyDown = (e, addTodo, setValue) => {
+const handleChange = (e, addTodo, setValue) => {
     if (e.target.value.trim().length === 0) {
-        return;
-    }
-    if (e.key === "Enter") {
-        addTodo(e.target.value);
-        e.target.value = "";
-        setValue("");
         return;
     }
     setValue(e.target.value);
@@ -23,13 +17,15 @@ const handleSubmit = (e, addTodo, setValue, inputRef) => {
     inputRef.current.value = "";
 }
 
+// 자주 리렌더 되는건 묶어 놓는게 효과적
 export function TodoInput({ addTodo }) {
     const [value, setValue] = useState("");
     const inputRef = useRef();
     return (
         <form onSubmit={(e) => handleSubmit(e, addTodo, setValue, inputRef)}>
-            <label htmlFor={"input_id"}>{value}</label>
-            <input id={"input_id"} ref={inputRef} onKeyDown={(e) => handleKeyDown(e, addTodo, setValue)}/>
+            {/*<label className={styles.input__monitor} htmlFor={"input_id"}>{value}</label>*/}
+            {/*<div className={styles.input__monitor}>{value}</div>*/}
+            <input id={"input_id"} ref={inputRef} onChange={(e) => handleChange(e, addTodo, setValue)}/>
             <button >add</button>
         </form>
     );
