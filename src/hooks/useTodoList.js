8ec70e6b1e0ba07fbdd53a226f328e3abb7c4970
JaeAnHan <jaean1999@naver.com>
2024-06-01 11:44:46 +0900

    feat: calc id and active count on reading cache

--- src/hooks/useTodoList.js
+++ src/hooks/useTodoList.js
@@ -1,6 +1,6 @@
 import {useCallback, useEffect, useState} from "react";
 // closure? 일단 전역 객체는 아님
-let id = 0;
+let idCnt = -1;
 let activeCnt = 0;
 
 const increaseActiveCnt = () => activeCnt += 1;
@@ -8,7 +8,7 @@ const decreaseActiveCnt = () => activeCnt -= 1;
 
 const createTodoInstance = (todo) => {
     increaseActiveCnt();
-    return {id: id++, content: todo, isDone: false};
+    return {id: idCnt++, content: todo, isDone: false};
 };
 
 const newTodoInstance = (todo, id, revertedIsDone) => {
@@ -33,13 +33,33 @@ const cacheTodo = (todos) => {
 
 const readCache = () => JSON.parse(localStorage.getItem("todos"));
 
+const cacheOnInit = () => {
+    const cached = readCache();
+    cached.forEach((todo) => {
+        idCnt = Math.max(idCnt, todo.id);
+        activeCnt += todo.isDone ? 0 : 1;
+    })
+    idCnt += 1;
+    return cached;
+};
+
+const filterAndDecreaseIfDone = (todo, id) => {
+    if (todo.id === id) {
+        if (!todo.isDone) decreaseActiveCnt();
+        return null;
+    }
+    return todo;
+}
+
 export function useTodoList() {
     const [todos, setTodos] = useState(readCache() ?? []);
     const addTodo = useCallback((todo) => setTodos(prev => [...prev, createTodoInstance(todo)]), []);
     const updateTodoState = useCallback(
         (id) => setTodos(prev => prev.map(todo => updateTodoStateImmutable(todo, id)))
         , []);
-    const deleteTodo = useCallback((id) => setTodos(prev => prev.filter(todo => todo.id !== id)), []);
+    const deleteTodo = useCallback(
+        (id) => setTodos(prev => prev.filter(todo => filterAndDecreaseIfDone(todo, id)))
+        , []);
     useEffect(() => cacheTodo(todos), [todos]);
 
     return { todos, addTodo, updateTodoState, deleteTodo, activeCnt };
