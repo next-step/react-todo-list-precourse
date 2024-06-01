87d54d96b2302c7c5c27748e086d5dd1588eb823
jihoon7171 <jihoon7171@naver.com>
2024-06-01 23:17:22 +0900

    [Add] list style

--- src/component/List.jsx
+++ src/component/List.jsx
@@ -1,5 +1,16 @@
+import TodoThing from "./TodoThing";
+import "./List.css"
 const List = () => {
-    return <div>List</div>
+    return <div className="List">
+        <h3>List</h3>
+        <div className="TodoThingsList">
+            <TodoThing/>
+            <TodoThing/>
+            <TodoThing/>
+        </div>
+
+
+    </div>
 };
 
 export default List;
---
+++ src/component/TodoThing.jsx
@@ -0,0 +1,12 @@
+
+import "./TodoThing.css"
+const TodoThing = () => {
+    return (
+        <div className="TodoThing">
+        <input type="checkbox"></input>
+        <div className="item">item</div>
+        <button>삭제</button>
+        </div>
+    )
+}
+export default TodoThing
