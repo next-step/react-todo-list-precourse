ca5cd9c369ac639701729023920ecd8f327b4fdc
JaeAnHan <jaean1999@naver.com>
2024-06-01 12:23:25 +0900

    feat: add submit button on TodoInput(Form)
    now available of adding todo by pressing enter or click submit

--- src/components/todos/TodoInput.jsx
+++ src/components/todos/TodoInput.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import { useRef, useState } from "react";
 
 const handleKeyDown = (e, addTodo, setValue) => {
     if (e.target.value.trim().length === 0) {
@@ -13,12 +13,24 @@ const handleKeyDown = (e, addTodo, setValue) => {
     setValue(e.target.value);
 }
 
+const handleSubmit = (e, addTodo, setValue, inputRef) => {
+    e.preventDefault();
+    if ((inputRef?.current?.value?.trim()?.length ?? 0) === 0) {
+        return;
+    }
+    addTodo(inputRef.current.value);
+    setValue("");
+    inputRef.current.value = "";
+}
+
 export function TodoInput({ addTodo }) {
     const [value, setValue] = useState("");
+    const inputRef = useRef();
     return (
-        <Fragment>
+        <form onSubmit={(e) => handleSubmit(e, addTodo, setValue, inputRef)}>
             <label htmlFor={"input_id"}>{value}</label>
-            <input id={"input_id"} onKeyDown={(e) => handleKeyDown(e, addTodo, setValue)}/>
-        </Fragment>
+            <input id={"input_id"} ref={inputRef} onKeyDown={(e) => handleKeyDown(e, addTodo, setValue)}/>
+            <button >add</button>
+        </form>
     );
 }
