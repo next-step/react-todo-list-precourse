3e7742885469fd5906b5b76b3d17d2a4bbccea84
JaeAnHan <jaean1999@naver.com>
2024-06-02 01:59:52 +0900

    refactor(TodoItem): vanilla jsx element codes to Component
    div -> Content
    input -> VariableContent
    button -> TodoButton

--- src/components/todos/TodoItem.jsx
+++ src/components/todos/TodoItem.jsx
@@ -1,5 +1,23 @@
 import { memo, useState } from "react";
 
+const switchToInput = (setIsEditing, isDone) => {
+    if (isDone) {
+        return;
+    }
+    setIsEditing(true);
+}
+
+function Content({ isDone, setIsEditing, content }) {
+    return (
+        <div
+            style={{textDecoration: isDone ? "line-through" : ""}}
+            onDoubleClick={() => switchToInput(setIsEditing, isDone)}
+        >
+            {content}
+        </div>
+    );
+}
+
 const handleKeyDown = (e, updateTodoState, oldTodo, setIdEditing) => {
     if (e.target.value.trim().length > 0 && e.key === "Enter") {
         updateTodoState({...oldTodo, content: e.target.value});
@@ -8,18 +26,45 @@ const handleKeyDown = (e, updateTodoState, oldTodo, setIdEditing) => {
     }
 }
 
+const handleBlue = (e, setIsEditing) => {
+    e.preventDefault();
+    setIsEditing(false);
+    e.target.value = "";
+}
+
+function VariableContent({ updateTodoState, todo, setIsEditing}) {
+    return (
+        <input
+            autoFocus={true}
+            onKeyDown={(e) => handleKeyDown(e, updateTodoState, todo, setIsEditing)}
+            onBlur={(e) => handleBlue(e, setIsEditing)}
+            title="press enter to submit"
+        />
+    );
+}
+
+const triggerUpdate = (updateTodoState, todo, isDone) => () => updateTodoState({...todo, isDone: !isDone});
+
+const triggerDelete = (deleteTodo, id) => () => deleteTodo(id)
+
+function TodoButton({ handleOnClick, text }) {
+    return (
+        <button onClick={handleOnClick}>{text}</button>
+    );
+}
+
 function TodoItem({ todo, updateTodoState, deleteTodo } ){
-    const [isEditing, setIdEditing] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
     const { id, content, isDone } = todo;
     console.log(id);
     return (
         <div>
             {isEditing
-                ? <input autoFocus={true} onKeyDown={(e) => handleKeyDown(e, updateTodoState, todo, setIdEditing)} />
-                : <div onDoubleClick={() => setIdEditing(true)}>{content}</div>
+                ? <VariableContent updateTodoState={updateTodoState} todo={todo} setIsEditing={setIsEditing} />
+                : <Content isDone={isDone} setIsEditing={setIsEditing} content={content} />
             }
-            <button onClick={() => updateTodoState({...todo, isDone: !isDone})}>{isDone ? 'isDone!' : 'notYet!'}</button>
-            <button onClick={() => deleteTodo(id)}>delete</button>
+            <TodoButton handleOnClick={triggerUpdate(updateTodoState, todo, isDone)} text={isDone ? 'isDone!' : 'notYet!'} />
+            <TodoButton handleOnClick={triggerDelete(deleteTodo, id)} text="delete" />
         </div>
     );
 }
