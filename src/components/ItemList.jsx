504e4dd5c8798ad9c90bf0b36559b2176724565e
minji2219 <200146@jnu.ac.kr>
2024-06-02 01:34:20 +0900

    Feat: 필터링에 따른 item출력

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,14 +1,25 @@
 import React, { useContext } from "react";
 import Item from "./Item";
 import { TodosContext } from "../context/TodosContext";
+import { FilterContext } from "../context/FilterContext";
 
 const ItemList = () => {
   const { todos } = useContext(TodosContext);
+  const { viewState } = useContext(FilterContext);
   return (
     <div>
-      {todos?.map((todo) => (
-        <Item key={todo.id} todoItem={todo} />
-      ))}
+      {todos?.map((todo) => {
+        if (viewState === "All") return <Item key={todo.id} todoItem={todo} />;
+        else if (viewState === "Active") {
+          if (!todo.checked) {
+            return <Item key={todo.id} todoItem={todo} />;
+          }
+        } else {
+          if (todo.checked) {
+            return <Item key={todo.id} todoItem={todo} />;
+          }
+        }
+      })}
     </div>
   );
 };
