21358313a02afb961cb7e0484e7d6eda5935417e
faddishcorn <juni129461@gmail.com>
2024-06-03 04:49:52 +0900

    feat: 완료 여부 토글

--- src/utils/todoUtils.js
+++ src/utils/todoUtils.js
@@ -1,3 +1,10 @@
+// 투두 관리도구
+export const toggleTodo = (todos, setTodos, index) => { //완료 여부 토글
+  const newTodos = [...todos];
+  newTodos[index].completed = !newTodos[index].completed;
+  setTodos(newTodos);
+};
+
 export const addTodo = (todos, setTodos, text) => { // 투두 추가
   if (!text.trim()) return; // 내용물 없으면 추가 방지
   setTodos([...todos, { text, completed: false }]);
