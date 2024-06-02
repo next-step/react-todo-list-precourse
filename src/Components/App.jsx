e2d7d87fc328f93b749c5905008f3dd2b295db2f
eunjin <eunjin0321@naver.com>
2024-06-02 23:42:07 +0900

    add: addTodo 함수 생성

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -3,6 +3,14 @@ import Header from './Header';
 import Footer from './Footer';
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [filter, setFilter] = useState('all');
+  
+  const addTodo = (text) => {
+    const newTodo = { text, completed: false, id: Date.now() };
+    console.log('Adding todo:', newTodo);
+    setTodos([...todos, newTodo]);
+  };
 
   return (
     <div>
