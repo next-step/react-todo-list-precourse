c5e2165ea7ee01ecd9ee92b3051d5970295d1339
mingkyeongg <ming0820@naver.com>
2024-06-02 23:19:38 +0900

    feat: create todo-list

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ToDoInput from './components/ToDoInput';
+import ToDoList from './components/ToDoList';
 
 const App = () => {
 	const [todos, setTodos] = useState([]);
@@ -12,6 +13,7 @@ const App = () => {
 		<div>
 			<h1>To-Do List</h1>
 			<ToDoInput addTodo={addTodo} />
+			<ToDoList todos={todos} />
 		</div>
 	);
 };
