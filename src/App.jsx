295902cfe530317246792c66313ade8b10bfaf00
JaeAnHan <jaean1999@naver.com>
2024-06-02 15:05:48 +0900

    feat(App): apply css style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Header } from "./components/Header.jsx";
 import { TodoList } from "./components/TodoList.jsx";
 import { Footer } from "./components/Footer.jsx";
+import styles from "./App.module.css";
 
 export function App() {
     console.log('app')
     return (
-        <div className="App">
+        <div className={styles.App}>
             <Header />
             <TodoList />
             <Footer />
