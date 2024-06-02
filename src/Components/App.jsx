72fba5fb4c2a8c9ab1b455686cd0905d95e93034
eunjin <eunjin0321@naver.com>
2024-06-03 00:01:02 +0900

    Feat: TodoFilter.jsx 생성

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -3,6 +3,8 @@ import Header from './Header';
 import Footer from './Footer';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
+import TodoFilter from './TodoFilter';
+
 
 
 
@@ -27,7 +29,7 @@ const App = () => {
   const deleteTodo = (id) => {
     setTodos(todos.filter(todo => todo.id !== id));
   };
-  
+
   const filteredTodos = todos.filter((todo) => {
     if (filter === 'all') return true;
     if (filter === 'active') return !todo.completed;
@@ -35,12 +37,23 @@ const App = () => {
     return true;
   });
 
+  const listNum = {
+    all: todos.length,
+    active: todos.filter(todo => !todo.completed).length,
+    completed: todos.filter(todo => todo.completed).length
+  };
+
+  const clearCompleted = () => {
+    setTodos([]);
+  };
+
   return (
     <div>
       <Header />
       <div className='todoForm'>
         <TodoForm addTodo={addTodo} />
-        <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+        <TodoList todos={filteredTodos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+        <TodoFilter setFilter={setFilter} listNum={listNum} filter={filter} clearCompleted={clearCompleted} />
       </div>
       <Footer />
     </div>
