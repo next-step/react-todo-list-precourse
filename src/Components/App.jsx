cec9b10bcb10197e32920497c83e9337e7ebf816
eunjin <eunjin0321@naver.com>
2024-06-02 23:58:19 +0900

    Add: filterTodos 함수 생성

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -27,6 +27,13 @@ const App = () => {
   const deleteTodo = (id) => {
     setTodos(todos.filter(todo => todo.id !== id));
   };
+  
+  const filteredTodos = todos.filter((todo) => {
+    if (filter === 'all') return true;
+    if (filter === 'active') return !todo.completed;
+    if (filter === 'completed') return todo.completed;
+    return true;
+  });
 
   return (
     <div>
