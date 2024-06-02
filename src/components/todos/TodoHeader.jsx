05c1598ee07a3de411c23e5108e298678ce2031f
JaeAnHan <jaean1999@naver.com>
2024-06-02 22:34:51 +0900

    feat(TodoHeader): add checkDefaultOnMount
    manually check TodoFilterButton("All") on mount not all checkboxes

--- src/components/todos/TodoHeader.jsx
+++ src/components/todos/TodoHeader.jsx
@@ -1,20 +1,23 @@
 import { PATH_ACTIVE, PATH_ALL, PATH_COMPLETED } from "../../constants.js";
 import { TodoFilterButton } from "./TodoFilterButton.jsx";
-import { memo, useRef } from "react";
+import { memo, useEffect, useRef } from "react";
 import styles from "../../styles/todos/TodoHeader.module.css"
 
 const paths = [PATH_ALL, PATH_ACTIVE, PATH_COMPLETED];
 
-const uncheckOthers= (refs) => (me) => refs.forEach((ref, i) => {
+const uncheckOthers = (refs) => (me) => refs.forEach((ref, i) => {
     if (me !== paths[i]) {
         refs[i].current.checked = false;
     }
-})
+});
+
+const checkDefaultOnMount = (refs) => () => { refs[0].current.checked = true };
 
 //TodoList로부터 path를 props로 받아오면 그냥 전부다 계속 리렌더됨
 function TodoHeader({ updatePathName }) {
     console.log('header');
     const refs = [useRef(), useRef(), useRef()];
+    useEffect(checkDefaultOnMount(refs), []);
     return (
         <div className={styles.filter}>
             {paths.map((path, i) => <TodoFilterButton
