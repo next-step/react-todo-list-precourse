fde42c4fc4786936a8457f9c7684538df9542353
JaeAnHan <jaean1999@naver.com>
2024-06-02 15:33:31 +0900

    feat(TodoHeader): apply css style
    add TodoHeader.module.css

--- src/components/todos/TodoHeader.jsx
+++ src/components/todos/TodoHeader.jsx
@@ -1,11 +1,13 @@
 import { PATH_ACTIVE, PATH_ALL, PATH_COMPLETED } from "../../constants.js";
 import { RouteButton } from "../../commons/RouteButton.jsx";
 import { memo } from "react";
+import styles from "../../styles/todos/TodoHeader.module.css"
 
 function TodoHeader({ updatePathName }) {
     const paths = [PATH_ALL, PATH_ACTIVE, PATH_COMPLETED];
+    console.log('header');
     return (
-        <ul>
+        <ul className={styles.filter}>
             {paths.map(path => <li key={path}><RouteButton path={path} updatePathName={updatePathName} /></li>)}
         </ul>
     );
