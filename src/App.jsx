8604ecad902561237ccfd8f301670dc819fb15a9
seorinnn <105342023+seorinnn@users.noreply.github.com>
2024-06-03 00:52:28 +0900

    feat: UI 구성

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="inner">
+        <h1>todos</h1>
+        <input type="text" placeholder="what needs to be done?" />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+const app = ReactDOM.createRoot(document.getElementById("app"));
+app.render(<App />);
