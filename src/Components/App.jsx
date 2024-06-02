17f375c5664c8eb69b7310ebedf20a16fcf32aa4
eunjin <eunjin0321@naver.com>
2024-06-02 23:40:09 +0900

    feat: Footer 함수 생성

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import Header from './Header';
+import Footer from './Footer';
 
 const App = () => {
 
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 };
+
+export default App;
