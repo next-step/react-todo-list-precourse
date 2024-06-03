7a7a76c6a11f4b7feef8a8781d18249ac8a483d2
DingX2 <pourding@naver.com>
2024-06-03 10:47:02 +0900

    feat(todocard): todocard UI 구현

--- src/components/organisms/TodoCard.tsx
+++ src/components/organisms/TodoCard.tsx
@@ -1,22 +1,57 @@
-import { type FC } from "react";
+import { type FC, useState } from "react";
 import {
   Box,
   Checkbox,
   Stack,
+  Typography,
   FormGroup,
   FormControlLabel,
+  IconButton,
 } from "@mui/material";
+import ClearIcon from "@mui/icons-material/Clear";
 
-export const TodoCard: FC = () => {
+interface Props {
+  id: number;
+  label: string;
+  onClick: (id: number) => void;
+}
+
+export const TodoCard: FC<Props> = ({ id, label, onClick }) => {
+  const [hoveredId, setHoveredId] = useState<number | null>(null);
+  const [checked, setChecked] = useState<boolean>(false);
+
+  const handleCheckboxChange = () => {
+    setChecked(!checked);
+  };
+
   return (
     <Box>
-      <Stack direction="column">
-        <FormGroup>
-          <FormControlLabel control={<Checkbox />} label="밥먹기" />
-        </FormGroup>
-        <FormGroup>
-          <FormControlLabel control={<Checkbox />} label="밥먹기" />
-        </FormGroup>
+      <Stack
+        direction="row"
+        alignItems="center"
+        onMouseEnter={() => setHoveredId(id)}
+        onMouseLeave={() => setHoveredId(null)}
+      >
+        <FormControlLabel
+          control={
+            <Checkbox checked={checked} onChange={handleCheckboxChange} />
+          }
+          label={
+            <Typography
+              sx={{
+                textDecoration: checked ? "line-through" : "none",
+                color: checked ? "gray" : "inherit",
+              }}
+            >
+              {label}
+            </Typography>
+          }
+        />
+        {hoveredId === id && (
+          <IconButton onClick={() => onClick(id)} aria-label="delete">
+            <ClearIcon />
+          </IconButton>
+        )}
       </Stack>
     </Box>
   );
