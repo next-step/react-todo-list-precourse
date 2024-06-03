5f656221fc73ceb2224fd9cdb53379891ed59268
DingX2 <pourding@naver.com>
2024-06-03 11:07:08 +0900

    feat(todoresultcard): 목록이 없는 경우 보이지 않게 구현

--- src/components/organisms/TodoResultCard.tsx
+++ src/components/organisms/TodoResultCard.tsx
@@ -13,37 +13,39 @@ export const TodoResultCard: FC<Props> = ({ leftItem }) => {
   };
 
   return (
-    <Stack direction="row" justifyContent={"space-between"}>
-      <Typography>{leftItem}</Typography>
+    leftItem && (
+      <Stack direction="row" justifyContent={"space-between"}>
+        <Typography>{leftItem} items left!</Typography>
 
-      <Stack direction="row" alignItems="center" spacing={2}>
-        {["All", "Active", "Completed"].map((filter) => (
-          <Button
-            key={filter}
-            onClick={() => handleFilterClick(filter)}
-            variant="text"
-            sx={{
-              border: activeFilter === filter ? "1px solid black" : "none",
-              borderRadius: 0,
-              padding: "2px 4px",
-              minWidth: "auto",
-            }}
-          >
-            {filter}
-          </Button>
-        ))}
+        <Stack direction="row" alignItems="center" spacing={2}>
+          {["All", "Active", "Completed"].map((filter) => (
+            <Button
+              key={filter}
+              onClick={() => handleFilterClick(filter)}
+              variant="text"
+              sx={{
+                border: activeFilter === filter ? "1px solid black" : "none",
+                borderRadius: 0,
+                padding: "2px 4px",
+                minWidth: "auto",
+              }}
+            >
+              {filter}
+            </Button>
+          ))}
+        </Stack>
+
+        <Typography
+          sx={{
+            cursor: "pointer",
+            "&:hover": {
+              textDecoration: "underline",
+            },
+          }}
+        >
+          Clear completed
+        </Typography>
       </Stack>
-
-      <Typography
-        sx={{
-          cursor: "pointer",
-          "&:hover": {
-            textDecoration: "underline",
-          },
-        }}
-      >
-        Clear completed
-      </Typography>
-    </Stack>
+    )
   );
 };
--- src/components/templates/TodoTemplate.tsx
+++ src/components/templates/TodoTemplate.tsx
@@ -1,15 +1,29 @@
-import { type FC } from "react";
-import { Input, Stack, Box } from "@mui/material";
+import { type FC, useState } from "react";
+import { Input, Stack, Typography, Container } from "@mui/material";
 import { TodoCard } from "../organisms/TodoCard";
 import { TodoResultCard } from "../organisms/TodoResultCard";
+import { type TodoItem } from "../types/todoItem";
 
 export const TodoTemplate: FC = () => {
+  const [todos, setTodos] = useState<TodoItem[]>([
+    { id: 1, label: "밥먹기" },
+    { id: 2, label: "운동하기" },
+  ]);
+
+  const handleDelete = (id: number) => {};
+
   return (
-    <Box>
-      <text>Todos</text>
+    <Container>
+      <Typography>Todos</Typography>
       <Input placeholder="What needs to be done?" />
-      <TodoCard />
-      <TodoResultCard />
-    </Box>
+
+      <Stack spacing={2}>
+        {todos.map(({ id, label }) => (
+          <TodoCard key={id} id={id} label={label} onClick={handleDelete} />
+        ))}
+      </Stack>
+
+      <TodoResultCard leftItem={1} />
+    </Container>
   );
 };
