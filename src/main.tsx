947fafa1db30d23969a49b5b65ee04e6c96a9d56
ajin <aj5210@naver.com>
2024-06-03 23:50:52 +0900

    Refactor: 프로그램 시작점 App.ts로 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,3 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { TodosProvider } from "./contexts/todos/TodosProvider.tsx";
+import { createApp } from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <TodosProvider>
-    <App />
-  </TodosProvider>,
-);
+createApp()
