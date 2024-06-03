1748bae7a71a621e6009d44bf9f80912d37dcebd
JeongSeoyun <xeonj@naver.com>
2024-06-03 09:59:02 +0900

    feat: Filter 컴포넌트 추가

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,10 +2,13 @@ import React, { useState } from 'react'
 import './Main.css'
 import TodoList from './TodoList'
 import AddTodo from './AddTodo';
+import Filter from './Filter';
 
 const Main = () => {
 
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([])
+  const [filter, setFilter]= useState('all')
+  const activeCount = todos.filter(todo => !todo.checked).length
 
   const addTodo = (todo) => {
     setTodos([...todos, { id: todos.length+1, text: todo, checked: false }])
@@ -31,11 +34,22 @@ const Main = () => {
     )
   }
 
+  const filteredTodo = (todos.filter(todo => {
+    if (filter === 'active') return todo.checked
+    else if (filter === 'completed') return !todo.checked
+    return true
+  }
+
+  ))
+
+  
+
   return (
     <div className='Main'>
       <div className='title'>todos</div>
       <AddTodo addTodo={addTodo} />
-      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} editTodo={editTodo} />
+      <TodoList todos={filteredTodo} toggleTodo={toggleTodo} deleteTodo={deleteTodo} editTodo={editTodo} />
+      <Filter activeCount={activeCount} setFilter={setFilter} filter={filter} />
     </div>
   )
 }
