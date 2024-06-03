22feec79a52621470302a7989d2a18794f003aec
cla6shade <cla6shade@github.com>
2024-06-03 09:26:46 +0900

    feat: move various hooks into useTodoHooks

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,5 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
-import Todo from '../../models/Todo';
+import { useState } from 'react';
 
 import styles from '../../css/todo.module.css';
 import TodoInput from './TodoInput';
@@ -7,60 +6,22 @@ import TodoFilterEnum from '../../constants/TodoFilterEnum';
 import TodoButtonBar from './TodoButtonBar';
 import TodoBox from './TodoBox';
 import { loadActiveCount, loadTodos } from '../../utils/Utils';
-import TodoStorageKeys from '../../constants/TodoStorageKeys';
 import { TodoRepository } from '../../utils/Types';
+import {
+  useCompleteRemove, useHandleCompletion,
+  useSaveTodos,
+  useTodoModification,
+} from '../../hooks/useTodoHooks';
 
 const TodoContainer = () => {
   const [todos, setTodos] = useState<TodoRepository>(loadTodos());
   const [activeCount, setActiveCount] = useState(loadActiveCount());
   const [filter, setFilter] = useState<TodoFilterEnum>(TodoFilterEnum.TODO_FILTER_ALL);
 
-  useEffect(() => {
-    sessionStorage.setItem(TodoStorageKeys.TODOS_JSON_KEY, JSON.stringify(todos));
-  }, [todos]);
-
-  useEffect(() => {
-    sessionStorage.setItem(TodoStorageKeys.TODOS_ACTIVE_COUNT_KEY, activeCount.toString());
-  }, [activeCount]);
-
-  const addTodo = useCallback((content: string) => {
-    const todo = new Todo(content);
-    setTodos({...todos, [todo.id]: todo});
-    setActiveCount(activeCount + 1);
-  }, [todos, activeCount]);
-
-  const removeTodo = useCallback((id: number) => {
-    const nextState = {...todos};
-    const todo = todos[id];
-    const activeCountDelta = todo.isCompleted ? 0 : -1;
-    setActiveCount(activeCount + activeCountDelta);
-    delete nextState[id];
-    setTodos(nextState);
-  }, [todos, activeCount]);
-
-  const pushIdIfComplete = ((ids: number[], todo: Todo) => {
-    if(todo.isCompleted) ids.push(todo.id);
-  });
-
-  const getCompletedTodoIds = useCallback(() => {
-    const ret: number[] = [];
-    Object.values(todos).forEach((todo) => pushIdIfComplete(ret, todo));
-    return ret;
-  }, [todos]);
-
-  const removeCompleteTodos = useCallback(() => {
-    const todoIds = getCompletedTodoIds();
-    const completeCount = todoIds.length;
-    const nextState = {...todos};
-    todoIds.map((todoId) => { delete nextState[todoId]; });
-    setTodos(nextState);
-    setActiveCount(activeCount - completeCount);
-  }, [getCompletedTodoIds]);
-
-  const handleToggleCompletion = useCallback((todo: Todo) => {
-    const activeCountDelta = todo.isCompleted ? -1 : 1;
-    setActiveCount(activeCount + activeCountDelta);
-  }, [activeCount]);
+  useSaveTodos(todos, activeCount);
+  const { addTodo, removeTodo } = useTodoModification({ todos, setTodos, activeCount, setActiveCount });
+  const { removeCompleteTodos } = useCompleteRemove({ todos, setTodos, activeCount, setActiveCount });
+  const { handleToggleCompletion } = useHandleCompletion({ activeCount, setActiveCount });
 
   return <div className={styles.container}>
     <TodoInput addTodo={addTodo} />
