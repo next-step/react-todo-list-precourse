cdd49c0a2c2b731fd51172b2c4c359c14d4ed008
Dobbymin <gmin0701@knu.ac.kr>
2024-06-04 16:33:27 +0900

    Feat: 할 일 작성 및 추가 기능 구현

--- src/components/input-list/ListContents.tsx
+++ src/components/input-list/ListContents.tsx
@@ -1,21 +1,53 @@
-import FoldIcon from '../../assets/Check-Icon.svg';
+import React, { useState } from 'react';
 import TodoContents from '../todo-list/TodoContents';
 import './ListContents.css';
+import Footer from '../footer/Footer';
+import { ListContentsProps } from './types';
+import useTodoFilter from '../../hooks/useTodoFilter';
+import InputSection from '../input-section/InputSection';
 
-const ListContents = () => {
+const ListContents: React.FC<ListContentsProps> = () => {
+	const [inputValue, setInputValue] = useState<string>('');
+	const {
+		todos,
+		filteredTodos,
+		addTodo,
+		handleCheckChange,
+		handleFilterChange,
+		handleClearCompleted,
+		toggleAllTodos,
+		checkedCount,
+		deleteTodo,
+	} = useTodoFilter([]);
+
+	const handleToggleAll = () => {
+		toggleAllTodos(true);
+	};
+
 	return (
 		<>
-			<div className="ListLayout">
-				<button>
-					<img src={FoldIcon} alt="Fold Icon" />
-				</button>
-				<input
-					className="Input-Item"
-					type="text"
-					placeholder="What needs to be done?"
+			<InputSection
+				inputValue={inputValue}
+				setInputValue={setInputValue}
+				addTodo={addTodo}
+				handleToggleAll={handleToggleAll}
+			/>
+			{filteredTodos.map((todo, index) => (
+				<TodoContents
+					key={index}
+					text={todo.text}
+					isChecked={todo.isChecked}
+					onCheckChange={(isChecked) => handleCheckChange(index, isChecked)}
+					onDelete={() => deleteTodo(index)}
 				/>
-			</div>
-			<TodoContents />
+			))}
+			{todos.length > 0 && (
+				<Footer
+					number={todos.length - checkedCount}
+					onFilterChange={handleFilterChange}
+					onClearCompleted={handleClearCompleted}
+				/>
+			)}
 		</>
 	);
 };
