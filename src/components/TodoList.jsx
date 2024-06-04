b7ba8751094b3fa3cf2dfe8cfa45674b4b42beef
JaeAnHan <jaean1999@naver.com>
2024-06-04 13:07:12 +0900

    feat(TodoList): add drag and drop destination

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,34 +1,34 @@
-import { PATH_ACTIVE, PATH_ALL } from "../constants.js";
 import TodoHeader from "./todos/TodoHeader.jsx";
 import { TodoInput } from "./todos/TodoInput.jsx";
 import { useTodoList } from "../hooks/useTodoList.js";
 import { usePath } from "../hooks/usePath.js";
-import TodoItem from "./todos/TodoItem.jsx";
 import styles from "../styles/TodoList.module.css"
+import { useDragAndDrop } from "../hooks/useDragAndDrop.js";
+import { TodoItemsFilter } from "./todos/TodoItemsFilter.jsx";
+import { createPortal } from "react-dom";
+import { memo } from "react";
 
-const filterCompleted = (todos) => todos.filter(todo => todo.isDone === true);
-
-const filterActive = (todos) => todos.filter(todo => todo.isDone === false);
-
-const doFilter = (path, todos) => {
-    if (path === PATH_ALL) return todos;
-    if (path === PATH_ACTIVE) return filterActive(todos);
-    return filterCompleted(todos);
-}
+const ItemDropDestination = memo(function ({ top = 0 }) {
+    console.log('portal');
+    return (
+        createPortal(
+            <div className={styles.todolist_dragDest}></div>, document.body
+        )
+    )
+})
 
 export function TodoList() {
     const { path, setPath } = usePath();
-    const { todos, addTodo, updateTodoState, deleteTodo, activeCnt } = useTodoList();
+    const { todos, addTodo, updateTodoState, deleteTodo, activeCnt, rearrangeTodos } = useTodoList();
+    const { onDragStart, onDragOver, onDragEnd } = useDragAndDrop(rearrangeTodos);
     return (
         <div className={styles.todolist}>
             <TodoHeader updatePathName={setPath} />
             <TodoInput addTodo={addTodo} />
             {activeCnt + "개 남음"}
-            <div className={styles.todolist__itemWrap}>
-                {doFilter(path, todos)
-                    .map(todo => <TodoItem key={todo.id} todo={todo} updateTodoState={updateTodoState} deleteTodo={deleteTodo} />)
-                }
-            </div>
+            <TodoItemsFilter path={path} todos={todos} updateTodoState={updateTodoState} deleteTodo={deleteTodo} onDragStart={onDragStart} onDragOver={onDragOver} onDragEnd={onDragEnd} />
+            <ItemDropDestination top={0}/>
+            {/*<Portal top={0} />*/}
         </div>
     );
 }
