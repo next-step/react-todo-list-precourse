9d789f1a432ffdf038c0824bf35bfbd9ab5c8f23
Dobbymin <gmin0701@knu.ac.kr>
2024-06-04 16:35:49 +0900

    Feat: 할 일 관리 기능 구현 및 컴포넌트 분리

--- src/components/todo-list/TodoContents.tsx
+++ src/components/todo-list/TodoContents.tsx
@@ -1,26 +1,41 @@
-import Footer from '../footer/Footer';
+import React from 'react';
 import './TodoContents.css';
-
 import DeleteIcon from '../../assets/Delete-Icon.svg';
-const TodoContents = () => {
+import { TodoContentsProps } from './types';
+
+const TodoContents: React.FC<TodoContentsProps> = ({
+	text,
+	isChecked,
+	onCheckChange,
+	onDelete,
+}) => {
+	const handleCheckboxChange = () => {
+		onCheckChange(!isChecked);
+	};
+
+	const handleDeleteClick = () => {
+		onDelete();
+	};
+
 	return (
-		<>
-			<div className="TodoLayout">
-				<div className="TodoContainer">
-					<label className="checkbox_label">
-						<input type="checkbox" />
-						<span className="checkbox_icon"></span>
-					</label>
-					<div className="checkbox_text">
-						<div>할 일 1</div>
-						<div className="Image_container">
-							<img src={DeleteIcon} alt="삭제하기" />
-						</div>
+		<div className="TodoLayout">
+			<div className="TodoContainer">
+				<label className="checkbox_label">
+					<input
+						type="checkbox"
+						checked={isChecked}
+						onChange={handleCheckboxChange}
+					/>
+					<span className="checkbox_icon"></span>
+				</label>
+				<div className={`checkbox_text ${isChecked ? 'checked' : ''}`}>
+					<div>{text}</div>
+					<div className="Image_container" onClick={handleDeleteClick}>
+						<img src={DeleteIcon} alt="삭제하기" />
 					</div>
 				</div>
 			</div>
-			<Footer />
-		</>
+		</div>
 	);
 };
 
