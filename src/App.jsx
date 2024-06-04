c809f58e4b4d2d5c4d0053e352b739c0437285d8
sondonghun97 <ehfk11115@gmail.com>
2024-06-05 02:30:36 +0900

    Feat: input입력받기

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 function App() {
+    const [inputValue,setInputValue] = useState("")
+    const [todoList,setTodoList]=useState([])
+    const addItem =() =>{
+        console.log("here",inputValue)
+        setTodoList([...todoList,inputValue])
+    }
     return (
-        <h1>hi</h1>
+        <main>
+            <input value ={inputValue}type="text"
+            onChange = {(event)=>setInputValue(event.target.value)}/>
+            <button onClick={addItem}>추가</button>
+        </main>
     );
 }
-
 export default App;
