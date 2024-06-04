612269711a997d0ce1565a8d8fffedb149b4445a
JaeAnHan <jaean1999@naver.com>
2024-06-04 13:51:27 +0900

    feat(TodoHeader): move portal to TodoHeader

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -5,30 +5,17 @@ import { usePath } from "../hooks/usePath.js";
 import styles from "../styles/TodoList.module.css"
 import { useDragAndDrop } from "../hooks/useDragAndDrop.js";
 import { TodoItemsFilter } from "./todos/TodoItemsFilter.jsx";
-import { createPortal } from "react-dom";
-import { memo } from "react";
-
-const ItemDropDestination = memo(function ({ top = 0 }) {
-    console.log('portal');
-    return (
-        createPortal(
-            <div className={styles.todolist_dragDest}></div>, document.body
-        )
-    )
-})
 
 export function TodoList() {
     const { path, setPath } = usePath();
     const { todos, addTodo, updateTodoState, deleteTodo, activeCnt, rearrangeTodos } = useTodoList();
-    const { onDragStart, onDragOver, onDragEnd } = useDragAndDrop(rearrangeTodos);
+    const { onDragStart, onDragOver, onDragEnd, portalRef } = useDragAndDrop(rearrangeTodos);
     return (
         <div className={styles.todolist}>
-            <TodoHeader updatePathName={setPath} />
+            <TodoHeader updatePathName={setPath} portalRef={portalRef} />
             <TodoInput addTodo={addTodo} />
             {activeCnt + "개 남음"}
             <TodoItemsFilter path={path} todos={todos} updateTodoState={updateTodoState} deleteTodo={deleteTodo} onDragStart={onDragStart} onDragOver={onDragOver} onDragEnd={onDragEnd} />
-            <ItemDropDestination top={0}/>
-            {/*<Portal top={0} />*/}
         </div>
     );
 }
--- src/components/todos/TodoHeader.jsx
+++ src/components/todos/TodoHeader.jsx
@@ -1,7 +1,8 @@
 import { PATH_ACTIVE, PATH_ALL, PATH_COMPLETED } from "../../constants.js";
 import { TodoFilterButton } from "./TodoFilterButton.jsx";
-import { memo, useEffect, useRef } from "react";
+import { forwardRef, memo, useEffect, useRef } from "react";
 import styles from "../../styles/todos/TodoHeader.module.css"
+import { createPortal } from "react-dom";
 
 const paths = [PATH_ALL, PATH_ACTIVE, PATH_COMPLETED];
 
@@ -13,8 +14,17 @@ const uncheckOthers = (refs) => (me) => refs.forEach((ref, i) => {
 
 const checkDefaultOnMount = (refs) => () => { refs[0].current.checked = true };
 
+const ItemDropDestination = forwardRef(function ({...props}, ref) {
+    console.log('portal');
+    return (
+        createPortal(
+            <div className={styles.todolist_dragDest} ref={ref}></div>, document.body
+        )
+    )
+})
+
 //TodoList로부터 path를 props로 받아오면 그냥 전부다 계속 리렌더됨
-function TodoHeader({ updatePathName }) {
+function TodoHeader({ updatePathName, portalRef }) {
     const refs = [useRef(), useRef(), useRef()];
     useEffect(checkDefaultOnMount(refs), []);
     return (
@@ -26,6 +36,7 @@ function TodoHeader({ updatePathName }) {
                 ref={refs[i]}
                 uncheckOthers={uncheckOthers(refs)}/>)
             }
+            <ItemDropDestination ref={portalRef} />
         </div>
     );
 }
