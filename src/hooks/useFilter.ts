fe303df24077160db7426fa19f0c5717d8fd018a
Dobbymin <gmin0701@knu.ac.kr>
2024-06-04 16:34:15 +0900

    Feat: custom Hook 구현 및 기능별 분리

---
+++ src/hooks/useFilter.ts
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import { Todo } from './types';
+
+const useFilter = (todos: Todo[]) => {
+	const [filter, setFilter] = useState<string>('all');
+
+	const handleFilterChange = (newFilter: string) => {
+		setFilter(newFilter);
+	};
+
+	const filteredTodos = todos.filter((todo) => {
+		if (filter === 'active') return !todo.isChecked;
+		if (filter === 'completed') return todo.isChecked;
+		return true;
+	});
+
+	return {
+		filter,
+		handleFilterChange,
+		filteredTodos,
+	};
+};
+
+export default useFilter;
