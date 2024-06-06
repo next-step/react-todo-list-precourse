b593e8dd0b1b4f26b52fd51236877e8edec600df
harugi7 <gimme2721@gmail.com>
2024-06-06 19:57:32 +0900

    refactor: remove log

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import FilterList from "./todo/FilterList";
 
 function Footer({ todoList, setFilter }) {
-    console.log(todoList);
     return (
         <footer className="footer">
           <FilterList setFilter={setFilter} />
