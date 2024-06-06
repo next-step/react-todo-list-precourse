bd05a64577955e816c1548fda364754006c2f275
leedohyun <leedo573@gmail.com>
2024-06-07 01:50:02 +0900

    Feat(todo-input): 추가할 일 입력 기능 구현

---
+++ src/components/TodoInput.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+function TodoInput({ onAddTodo }) {
+  const [input, setInput] = useState("");
+
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (!input.trim()) {
+      alert("할 일을 입력해주세요!");
+      return;
+    }
+    onAddTodo(input);
+    setInput("");
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleSubmit();
+    }
+  };
+
+  return React.createElement(
+    "div",
+    null,
+    React.createElement("input", {
+      type: "text",
+      value: input,
+      onChange: handleChange,
+      onKeyDown: handleKeyDown,
+      placeholder: "What needs to be done?",
+      className: "todoInput",
+    }),
+    React.createElement(
+      "button",
+      {
+        onClick: handleSubmit,
+        className: "add-button",
+      },
+      "추가"
+    )
+  );
+}
+
+export default TodoInput;
