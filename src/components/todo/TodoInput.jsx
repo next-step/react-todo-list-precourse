cfc6faaaaae44b14d825ce2219e351835832e116
harugi7 <gimme2721@gmail.com>
2024-06-06 19:55:25 +0900

    refactor: change the structure of TodoInput

--- src/components/todo/TodoInput.jsx
+++ src/components/todo/TodoInput.jsx
@@ -4,13 +4,9 @@ function TodoInput({ text, onChangeInput, onSubmit }) {
     return (
         <form onSubmit={onSubmit}>
             <div className="input-container">
-              <input
-                className="new-todo"
-                id="todo-input"
-                type="text"
-                placeholder="what needs to be done?"
-                onChange={onChangeInput}
-                value={text}
+              <input className="new-todo" id="todo-input"
+                type="text" placeholder="what needs to be done?"
+                onChange={onChangeInput} value={text}
               ></input>
               <button className="new-todo-add" type="submit">add</button>
             </div>
