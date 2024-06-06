f25d30556ac146c2b0d75b93ecba06dd2f2e9cc2
harugi7 <gimme2721@gmail.com>
2024-06-06 19:51:59 +0900

    refactor: separate the components of Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,11 @@
 import React from "react";
+import TodoInput from "./todo/TodoInput";
 
 function Header({ text, onChangeInput, onSubmit }) {
     return (
         <header className="header">
           <h1>todos</h1>
-          <form onSubmit={onSubmit}>
-            <div className="input-container">
-              <input
-                className="new-todo"
-                id="todo-input"
-                type="text"
-                placeholder="what needs to be done?"
-                onChange={onChangeInput}
-                value={text}
-              ></input>
-              <button className="new-todo-add" type="submit">add</button>
-            </div>
-          </form>
+          <TodoInput text={text} onChangeInput={onChangeInput} onSubmit={onSubmit}/>
         </header>
     )
 }
---
+++ src/components/todo/TodoInput.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function TodoInput({ text, onChangeInput, onSubmit }) {
+    return (
+        <form onSubmit={onSubmit}>
+            <div className="input-container">
+              <input
+                className="new-todo"
+                id="todo-input"
+                type="text"
+                placeholder="what needs to be done?"
+                onChange={onChangeInput}
+                value={text}
+              ></input>
+              <button className="new-todo-add" type="submit">add</button>
+            </div>
+          </form>
+    );
+}
+
+export default TodoInput;
