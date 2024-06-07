946da5b491cc93f92c9b99fbe280cce7c66b84e9
jihoon7171 <jihoon7171@naver.com>
2024-06-07 17:08:58 +0900

    [Add label] todothing label추가

--- src/component/TodoThing.jsx
+++ src/component/TodoThing.jsx
@@ -1,10 +1,14 @@
-import React, { useCallback, useState } from "react"
+import React, { useEffect, useState } from "react"
 import "./TodoThing.css"
 const TodoThing = ({todo, onRemove, checkpush}) => {
-const [isChecked, setIsChecked] = useState(false);
+const [isChecked, setIsChecked] = useState(todo.isdone);
 const removeHandler = () => {
     onRemove(todo.id)
 }
+
+useEffect(() => {
+    setIsChecked(todo.isdone)
+},[todo.isdone])
 
 const checkHandler = () => {
     checkpush(todo.id, todo.content)
@@ -13,10 +17,11 @@ const checkHandler = () => {
 
     return (
         <div className="TodoThing">
-
+        <label>
         <input type="checkbox" checked={isChecked} onChange={checkHandler} ></input>
-        <div className="item">{todo.content}</div>
-        <button className="deletebtn"onClick={removeHandler}>삭제</button>
+        <span className="item">{todo.content}</span>
+        </label>
+       <button className="deletebtn"onClick={removeHandler}>삭제</button>
         </div>
     )
 }
