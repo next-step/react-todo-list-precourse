c8a0e07f4521e977cafe37eaa981be9dd3adb592
hojeong26 <rkdghwjd0926@jnu.ac.kr>
2024-06-07 12:25:24 +0900

    feat: Enter키나 버튼을 눌렀을 때 할일 저장할 수 있도록 state생성

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React,{useState} from 'react'
 import InputField from './components/InputField'
 // import TodoList from './components/TodoList';
 import Footer from './components/Footer'
 import './App.css'
 
 function App() {
+  const [todolist, setTodos] = useState([])
+  const addTodo = (todo) => {
+    setTodos([...todolist, todo])
+  }
   return React.createElement(
     'div', null,
     React.createElement(
       'header', null, React.createElement('p', null, 'todos')),
     React.createElement(
       'div', { className: 'todoListBody' },
-      React.createElement(InputField, null),
+      React.createElement(InputField, {addTodo})
     //   // React.createElement(TodoList, null) // 주석 처리된 부분이므로 오류가 발생할 수 있습니다.
     ),
     React.createElement(Footer, null)
--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Input from './Input'
 import './InputField.css'
 
-function InputField() {
+function InputField({addTodo}) {
+  const [inputValue, setInputValue] = useState('')
+
+  const inputChange = (event) => {
+    setInputValue(event.target.value)
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      console.log('Enter key pressed!');
+      todoSubmit()
+    }
+  };
+
+  const todoSubmit = () => {
+    if (inputValue.trim()) {
+      addTodo(inputValue);
+      setInputValue('')
+    }
+  };
+
   return React.createElement(
     'div',
     {className: 'inputContainer' },
-    React.createElement('input', {type: 'text', placeholder: 'What needs to be done?'}, null),
-    React.createElement('button', {className: 'addButton'}, 'ADD')
+    React.createElement(Input, {
+      inputValue, inputChange, handleKeyPress
+    }),
+    React.createElement('button', {className: 'addButton',onClick: todoSubmit}, 'ADD')
   )
 }
 
