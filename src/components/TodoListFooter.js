635a3f4fc62e2916b95fc440d2bf936264e8ae44
hojeong26 <rkdghwjd0926@jnu.ac.kr>
2024-06-07 16:42:08 +0900

    feat: clearCompleted함수를 이용해서 TodoListfooter의 clearBtn에 적용하여 completed된 리스트 지움

--- src/components/TodoListFooter.js
+++ src/components/TodoListFooter.js
@@ -13,11 +13,11 @@ const TodoListFooter = ({ todolist, setFilter, clearCompleted }) => {
     return React.createElement('div',{className: 'todoListFooterDiv' },
         React.createElement('p',null,`${activeTodocount} item left!`),
         React.createElement('div',{className:'listStateBtnDiv' },
-            React.createElement('button',{className:'listStateBtn'},'All'),
-            React.createElement('button',{className:'listStateBtn'},'Active'),
-            React.createElement('button',{className:'listStateBtn'},'Completed')
+            React.createElement('button',{className:'listStateBtn',onClick: () => setFilter('all')},'All'),
+            React.createElement('button',{className:'listStateBtn',onClick: () => setFilter('active')},'Active'),
+            React.createElement('button',{className:'listStateBtn',onClick: () => setFilter('completed')},'Completed')
         ),
-        React.createElement('button',{className:'clearBtn'},'Clear Completed')
+        React.createElement('button',{className:'clearBtn',onClick: clearCompleted},'Clear Completed')
     )
 }
 
