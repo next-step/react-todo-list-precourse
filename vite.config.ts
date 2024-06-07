d39df36ac7cdac9174317bccda82877c15c3731f
Solbin Jung <jsolbin0724@gmail.com>
2024-06-07 23:53:52 +0900

    rename: .jsx에서 App.js로 변경

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild:{
+    loader: 'jsx',
+    include: [
+      'src/**/*.js',
+      'src/**/*.jsx'
+    ],
+    exclude: [],
+  },
 })
