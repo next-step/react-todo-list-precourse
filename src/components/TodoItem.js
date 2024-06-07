eb7ecde50401478314b0452fbf03c01e992fc2e9
hojeong26 <rkdghwjd0926@jnu.ac.kr>
2024-06-07 21:59:45 +0900

    feat: todolist의 글자만 눌러도 체크되도록 구현

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,10 +5,11 @@ function TodoItem ({todo, index, toggleTodo, deleteTodo}) {
         React.createElement('input', {
           className: 'checkBox',
           type: 'checkbox',
+          style: { cursor: 'pointer' },
           checked: todo.completed,
           onChange: () => toggleTodo(index)
         }),
-        React.createElement('span', null, todo.text),
+        React.createElement('span', {onClick: () => toggleTodo(index),style: { cursor: 'pointer' }}, todo.text),
         React.createElement('button', {
           className: 'deleteButton',
           onClick: () => deleteTodo(index)
