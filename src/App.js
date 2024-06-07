faacd4fdcda58eaf2774fc68ee81a697a16d6ea7
hojeong26 <rkdghwjd0926@jnu.ac.kr>
2024-06-07 10:14:54 +0900

    refactor: footer부분 함수 컴포넌트로 리팩토링

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import InputField from './components/InputField'
+// import TodoList from './components/TodoList';
+import Footer from './components/Footer'
+import './App.css'
+
+function App() {
+  return React.createElement(
+    'div', null,
+    React.createElement(
+      'header', null, React.createElement('p', null, 'todos')),
+    React.createElement(
+      'div', { className: 'todoListBody' },
+      React.createElement(InputField, null),
+    //   // React.createElement(TodoList, null) // 주석 처리된 부분이므로 오류가 발생할 수 있습니다.
+    ),
+    React.createElement(Footer, null)
+  )
+}
+
+export default App;
