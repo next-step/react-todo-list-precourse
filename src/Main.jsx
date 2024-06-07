82617ad7dece6d371a7bd1bab6d3dde98a7b8477
TAEHONG-LEE <2t_hong@naver.com>
2024-06-08 04:43:22 +0900

    feat(2.1): add Footer

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,6 +2,7 @@ import "./Main.css";
 import React from "react";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import Footer from "./components/Footer";
 
 function Main() {
     return (
@@ -9,6 +10,7 @@ function Main() {
             <section className="todoapp">
                 <Header />
                 <Body />
+                <Footer />
             </section>
         </div>
     );
