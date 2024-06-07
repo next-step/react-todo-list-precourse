5b0797a9d6776b37ff7096ce028c85150f5e744c
Kuheewon <selly3109@naver.com>
2024-06-07 17:09:51 +0900

    feat. 완료 상태 전환

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import '../styles/TodoList.css';
 
+const getStyle = (completed) => ({
+  padding: "10px",
+  textDecoration: completed? "line-through" : "none"
+});
+
 function TodoList({data, setTodoData}) {
+
   const handleCompleteChange = (id) => {
-
-
+    setTodoData((prev) =>
+    prev.map((todo) =>
+      todo.id === id ? {...todo, completed: !todo.completed} : todo
+    ))
   }
 
   const handleClick = (id) => {
     setTodoData((prev) => prev.filter((data) => data.id !== id))
-
-
   }
+
   return (
-    <div className="todoList" key = {data.id}>
+    <div className="todoList" style = {getStyle(data.completed)} key = {data.id}>
       <div className="todo-item-wrapper">
         <input type="checkbox"
          checked={data.completed} 
