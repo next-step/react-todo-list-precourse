b3c65e460a840c09e5bf9d884ee8f66556e640b7
jihoon7171 <jihoon7171@naver.com>
2024-06-07 16:31:16 +0900

    [FIX NAME] maker -> todo maker 수정 , 15줄로 수정

--- src/component/Maker.jsx
+++ src/component/Maker.jsx
@@ -2,35 +2,25 @@ import React, {useState} from "react";
 import "./Maker.css"
 
 
-const Maker = ({pushthing}) => {
+const Todomaker = ({pushthing}) => {
 
     const [inputvalue, setinputvalue] = useState('')
 
-    const handlechange = (e) => {
-
-        setinputvalue(e.target.value);
-    }
+    const handlechange = (e) =>  setinputvalue(e.target.value);
     const handleSubmit = (e) => {
       e.preventDefault();
-      if (inputvalue === "") {
-        alert("할 일을 입력하세요📝");
-      } else {
+      if (inputvalue === "")  alert("할 일을 입력하세요📝");
+       else {
         pushthing(inputvalue);
         setinputvalue("");
       }
     };
 
     return (
-        <form className="Maker" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={inputvalue}
-            onChange={handlechange}
-            placeholder="할 일을 입력하세요"
-          />
-         
+        <form className="Todomaker" onSubmit={handleSubmit}>
+          <input type="text" value={inputvalue}  onChange={handlechange} placeholder="할 일을 입력하세요" />
         </form>
       );
 };
 
-export default Maker;
+export default Todomaker;
