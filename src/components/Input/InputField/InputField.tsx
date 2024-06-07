c9615daaa97bc50c24cd67b636c423328f5d3c0e
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-06-07 23:49:25 +0900

    fix: remove ToggleAll component

--- src/components/Input/InputField/InputField.tsx
+++ src/components/Input/InputField/InputField.tsx
@@ -1,26 +1,16 @@
 import styles from './InputField.module.css';
 import TextInput from '../TextInput/TextInput';
-import ToggleAll from '../ToggleAll/ToogleAll';
 
 export interface InputFieldProps {
   type: 'text' | 'checkbox';
   label: string;
   placeholder?: string;
   value?: string;
-  checked?: boolean;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
 }
 
-const InputField = ({
-  type,
-  placeholder,
-  label,
-  value,
-  checked,
-  onChange,
-  onKeyDown,
-}: InputFieldProps) => {
+const InputField = ({ type, placeholder, label, value, onChange, onKeyDown }: InputFieldProps) => {
   return (
     <div>
       {type === 'text' && (
@@ -31,15 +21,6 @@ const InputField = ({
           value={value}
           onChange={onChange}
           onKeyDown={onKeyDown}
-        />
-      )}
-      {type === 'checkbox' && (
-        <ToggleAll
-          type={type}
-          label={label}
-          value={value}
-          checked={checked ?? false}
-          onChange={onChange}
         />
       )}
       <label htmlFor={label} className={type === 'text' ? styles['sr-only'] : ''}>
