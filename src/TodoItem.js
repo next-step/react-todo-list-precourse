e273208025c3f5c7a9a25410f7c3aafc721b6024
Solbin Jung <jsolbin0724@gmail.com>
2024-06-08 00:23:05 +0900

    refactor: 체크박스 태그와 label 태그 함께 작성

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,10 +3,14 @@ import React from 'react';
 const TodoItem = ({ todo, toggleTodoCompletion, deleteTodo, setTodos, todos }) => {
   return (
     <li className={todo.completed ? 'todo completed' : 'todo'}>
-      <div className="checkbox" onClick={() => toggleTodoCompletion(todo.id, todos, setTodos)}>
-        {todo.completed ? '✔' : ''}
-      </div>
-      <label>{todo.text}</label>
+    <input
+        type="checkbox"
+        id={`todo-${todo.id}`}
+        className="checkbox"
+        checked={todo.completed}
+        onChange={() => toggleTodoCompletion(todo.id, todos, setTodos)}
+      />
+      <label htmlFor={`todo-${todo.id}`}>{todo.text}</label>
       <button className="destroy" onClick={() => deleteTodo(todo.id, todos, setTodos)}>×</button>
     </li>
   );
