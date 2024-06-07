59f3953aee7f83d434cd23d9c97a6b8d7b67c61c
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-06-07 16:18:01 +0900

    feat: Implement filtering for all todos

--- src/components/TodoList/TodoLIst.tsx
+++ src/components/TodoList/TodoLIst.tsx
@@ -6,10 +6,15 @@ import TodoContext from '../../context/TodoContext';
 const TodoList = () => {
   const value = useContext(TodoContext);
   const todoList = value?.state.todoList;
+  const filter = value?.state.filter;
+
+  const filteredTodoList = todoList?.filter(todoListItem => {
+    if (filter === 'all') return true;
+  });
 
   return (
     <ul className={styles['todo-list']}>
-      {todoList?.map(todoListItem => (
+      {filteredTodoList?.map(todoListItem => (
         <TodoListItem key={todoListItem.no} todoListItem={todoListItem} />
       ))}
     </ul>
