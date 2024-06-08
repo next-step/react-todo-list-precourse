3ebb01e6cab1b44c95091663644a62de22c915a0
ajin <aj5210@naver.com>
2024-06-08 20:38:06 +0900

    Refactor(useTodoActions): 함수 컴포넌트 분리

--- src/hooks/useTodoActions.ts
+++ src/hooks/useTodoActions.ts
@@ -1,24 +1,30 @@
+import { Dispatch } from "react";
 import { useTodos } from "../contexts/todos/useTodos";
+
+const addTodo = (dispatch: Dispatch<TodosAction>, text: string) => {
+  if (!text.trim()) return;
+  dispatch({ type: "ADD_TODO", text });
+};
+
+const toggleTodo = (dispatch: Dispatch<TodosAction>, id: number) => {
+  dispatch({ type: "TOGGLE_TODO", id });
+};
+
+const removeTodo = (dispatch: Dispatch<TodosAction>, id: number) => {
+  dispatch({ type: "REMOVE_TODO", id });
+};
+
+const removeDoneTodos = (dispatch: Dispatch<TodosAction>) => {
+  dispatch({ type: "REMOVE_DONE_TODOS" });
+};
 
 export const useTodoActions = () => {
   const { dispatch } = useTodos();
 
-  const addTodo = (text: string) => {
-    if (!text.trim()) return;
-    dispatch({ type: "ADD_TODO", text });
+  return {
+    addTodo: (text: string) => addTodo(dispatch, text),
+    toggleTodo: (id: number) => toggleTodo(dispatch, id),
+    removeTodo: (id: number) => removeTodo(dispatch, id),
+    removeDoneTodos: () => removeDoneTodos(dispatch),
   };
-
-  const toggleTodo = (id: number) => {
-    dispatch({ type: "TOGGLE_TODO", id });
-  };
-
-  const removeTodo = (id: number) => {
-    dispatch({ type: "REMOVE_TODO", id });
-  };
-
-  const removeDoneTodos = () => {
-    dispatch({ type: "REMOVE_DONE_TODOS" });
-  };
-
-  return { addTodo, toggleTodo, removeTodo, removeDoneTodos };
 };
