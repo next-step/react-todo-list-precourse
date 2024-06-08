d3188297e88eb0b9b03a4129b5b6c51d647c64b9
Eric <real.dassy@gmail.com>
2024-06-09 01:48:00 +0900

    feat: 컴포넌트 레이아웃 컴포넌트 배치
    - Title, TextField, FilterButton, TodoList 컴포넌트 임포트 및 사용
    - Title, TextField, FilterButton, TodoList 컴포넌트 임포트 배치
    - TextField에서 새로운 태스크 입력 시 tasks 배열에 추가하는 handleEnter 함수 구현
    - 필터 버튼 클릭 시 selectedRole 상태를 업데이트하는 handleFilterClick 함수 구현
    - 필터 버튼 클릭 시 선택된 버튼 스타일 적용

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import "./styles/App.css";
+import Title from "./components/Title";
+import TextField from "./components/TextField";
+import FilterButton from "./components/FilterButton";
+import TodoList from "./components/TodoList";
+import { React, useState } from "react";
+
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [selectedRole, setSelectedRole] = useState("All");
+
+  const handleEnter = (newTask) => {
+    setTasks([...tasks, newTask]);
+  };
+
+  const handleFilterClick = (role) => {
+    setSelectedRole(role);
+  };
+
+  return (
+    <div id="app">
+      <Title />
+      <TextField onEnter={handleEnter} />
+      <div className="filterButton-container">
+        <FilterButton role="All" selectedRole={selectedRole} onClick={handleFilterClick} />
+        <FilterButton role="Active" selectedRole={selectedRole} onClick={handleFilterClick} />
+        <FilterButton role="Completed" selectedRole={selectedRole} onClick={handleFilterClick} />
+      </div>
+      <TodoList tasks={tasks} />
+    </div>
+  );
+};
+
+export default App;
