1b1fd1f4c4918dee062a43a5a569e0b7ebe4476b
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-06-08 15:12:34 +0900

    refactor: add ClearCompletedButton component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import styles from './Footer.module.css';
 import TodoContext from '../../context/TodoContext';
 import Filters from './Filters/Filters';
+import ClearCompletedButton from './ClearCompletedButton/ClearCompletedButton';
 
 const Footer = () => {
   const value = useContext(TodoContext);
@@ -11,13 +12,7 @@ const Footer = () => {
     <footer className={styles.footer}>
       <span>{countIncompleteTodos} item left!</span>
       <Filters />
-      <button
-        type="button"
-        className={styles['clear-completed']}
-        onClick={() => value?.actions.clearCompleted()}
-      >
-        Clear completed
-      </button>
+      <ClearCompletedButton clearCompleted={value?.actions.clearCompleted} />
     </footer>
   );
 };
