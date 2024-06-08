17a5b7245951170ce2e2ba3645b1bdf35cb2ce79
ajin <aj5210@naver.com>
2024-06-08 20:17:08 +0900

    Refactor(TodoForm): 할 일 추가 함수 분리

--- src/components/header/TodoForm.tsx
+++ src/components/header/TodoForm.tsx
@@ -1,21 +1,27 @@
-import { useState } from "react";
+import { Dispatch, useState } from "react";
 import { useTodoActions } from "../../hooks/useTodoActions";
 import { TodoFormButton } from "./TodoFormButton";
 import { TodoFormInput } from "./TodoFormInput";
+
+const handleAddTodoClick = (
+  input: string,
+  addTodo: (text: string) => void,
+  setInput: Dispatch<React.SetStateAction<string>>,
+) => {
+  if (input.trim()) addTodo(input);
+  setInput("");
+};
 
 export const TodoForm = () => {
   const [input, setInput] = useState<string>("");
   const { addTodo } = useTodoActions();
 
-  const handleAddTodoClick = () => {
-    if (input.trim()) addTodo(input);
-    setInput("");
-  };
+  const onClick = () => handleAddTodoClick(input, addTodo, setInput);
 
   return (
     <form onSubmit={(e) => e.preventDefault()}>
       <TodoFormInput input={input} setInput={setInput} />
-      <TodoFormButton onClick={handleAddTodoClick} />
+      <TodoFormButton onClick={onClick} />
     </form>
   );
 };
