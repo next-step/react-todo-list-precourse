15817c2f9df2b0e35f97a27019a19e6d31ee547f
yeahzee0421 <ydw08250421@gmail.com>
2024-06-08 22:20:54 +0900

    style: update colors

--- src/components/Common/Header/Header.jsx
+++ src/components/Common/Header/Header.jsx
@@ -3,7 +3,7 @@ import image from "../../../assets/image/image.jpg";
 export default function Header() {
   return (
     <div className="header">
-      <img src={image} style={{ width: "120px" }} />
+      <img src={image} style={{ width: "140px" }} />
       <h1>오늘 할 일</h1>
     </div>
   );
