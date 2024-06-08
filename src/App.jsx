a16fab575726930bafd9fd69601690c1ea6fb1ad
YIMSEBIN <jij09123@gmail.com>
2024-06-09 01:24:47 +0900

    feat: 할 일 완료 상태 전환

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
+import TodoFilter from './TodoFilter';
 
 const App = () => {
   const [todos, setTodos] = useState([]);
+  const [filter, setFilter] = useState('all');
 
   const addTodo = (text) => {
     if (text === '') return;
@@ -14,13 +16,29 @@ const App = () => {
   const deleteTodo = (todoId) => {
     setTodos(todos.filter(todo => todo.id !== todoId ));
   };
+  
+  const filteredTodos = todos.filter(todo => {
+    if (filter === 'all') return true;
+    if (filter === 'completed') return todo.completed;
+    if (filter === 'active') return !todo.completed;
+    return true;
+  });
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
 
   return (
     <div className="App">
       <h1>TODO</h1>
       <TodoForm addTodo={addTodo} />
-      <TodoList todos={todos}
-                deleteTodo={deleteTodo}
+      <TodoList todos={filteredTodos}
+        deleteTodo={deleteTodo}
+        toggleTodo={toggleTodo}
       />
     </div>
   );
