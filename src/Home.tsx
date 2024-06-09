55ff0190cbc23685ea0b8d81bb1592106ac8a4ed
dandamdandam <glue0440@gmail.com>
2024-06-09 11:37:43 +0900

    feat: (Home.tsx) add todoList state

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,8 +1,30 @@
+import { useState, useEffect } from "react";
+import { Todo } from "./interfaces/todo";
+
 /**
  * Todo 홈페이지 컴포넌트
  * + todoList state 관리
  */
 const Home = () => {
+    const [todoList, setTodoList] = useState<Todo[]>([]);
+
+    // TODO 테스트 용 데이터 지우기
+    const dummyData: Todo[] = [
+        {
+            id: 1,
+            done: false,
+            content: 'make todo project'
+        },
+        {
+            id: 2,
+            done: true,
+            content: 'clean room'
+        },
+    ]
+    useEffect(() => {
+        setTodoList(dummyData);
+    },[])
+
     return(
         <div>
             dldl
