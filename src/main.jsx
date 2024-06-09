32766ea80de446621f1277cdb806b74c3a71ed36
seorinnn <105342023+seorinnn@users.noreply.github.com>
2024-06-09 23:55:19 +0900

    fix : 시작파일 및 확장자 변경

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import React, { useState } from "react";
+import "./App.css";
+import Input from "./components/Input.jsx";
+import TodoList from "./components/TodoList.jsx";
+import handleInputChange from "./utils/handleInputChange.jsx";
+import handleKeyPress from "./utils/handleKeyPress.jsx";
+import addTodo from "./utils/addTodo.jsx";
 
-const app = ReactDOM.createRoot(document.getElementById("app"));
-app.render(<App />);
+function Main() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  return (
+    <div className="inner">
+      <h1>todos</h1>
+      <Input
+        value={inputValue}
+        onChange={(e) => handleInputChange(e, setInputValue)}
+        onKeyPress={(e) =>
+          handleKeyPress(e, todos, setTodos, inputValue, setInputValue)
+        }
+      />
+      <TodoList todos={todos} />
+    </div>
+  );
+}
+
+export default Main;
