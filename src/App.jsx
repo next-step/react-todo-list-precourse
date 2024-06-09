cff7a37e7ee871b9742360815a9336a390e2aa23
wjdsbs <wjdsbss@gmail.com>
2024-06-10 05:59:58 +0900

    feat : 로컬스토리지 데이터 유지 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./styles/App.css";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import TodoList from "./components/TodoList";
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = JSON.parse(localStorage.getItem("todos-app"));
+    if (savedTodos != null) {
+      return savedTodos;
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("todos-app", JSON.stringify(todos));
+  }, [todos]);
 
   const addTodo = (todo) => {
     setTodos([...todos, { id: Date.now(), text: todo, completed: false }]);
