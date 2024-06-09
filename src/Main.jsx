a0d4db703e25377766b8a6c2536e50438bd474e1
soyeeee2 <soyeeee2@gmail.com>
2024-06-10 01:07:19 +0900

    setting: 세팅 재작업

---
+++ src/Main.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import React from "react";
+import useTodoState from "./components/hooks/useTodoState";
+import SubmitForm from "./components/ui/SubmitForm";
+import TodoList from "./components/ui/TodoList";
+import Footer from "./components/ui/Footer";
+
+function Main() {
+  window.addEventListener('load', () => {
+    localStorage.clear();
+  });
+
+  const { lists, setLists, inputRef, onSubmit, onDelete, onToggle, showConditionToggle, filteredTodoList } = useTodoState();
+
+  return (
+    <div id="root">
+      <section className="todoapp">
+        <SubmitForm onSubmit={onSubmit} inputRef={inputRef} />
+        <TodoList lists={lists} HandleToggleComplete={onToggle} HandleDeleteItem={onDelete} />
+        <Footer ShowCompletedToggle={showConditionToggle} lists={lists} ClearCompleted={filteredTodoList} />
+      </section>
+    </div>
+  );
+}
+
+export default Main;
