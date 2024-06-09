13d58847a71f5c199d210bd7621e0e14a1a82c0c
Dobbymin <gmin0701@knu.ac.kr>
2024-06-10 00:53:17 +0900

    Fix: Type 파일 경로 변경

--- src/hooks/useFilter.ts
+++ src/hooks/useFilter.ts
@@ -1,24 +1,24 @@
 import { useState } from 'react';
-import { Todo } from './types';
+import { Todo } from '../types/HookTypes';
 
 const useFilter = (todos: Todo[]) => {
-	const [filter, setFilter] = useState<string>('all');
+  const [filter, setFilter] = useState<string>('all');
 
-	const handleFilterChange = (newFilter: string) => {
-		setFilter(newFilter);
-	};
+  const handleFilterChange = (newFilter: string) => {
+    setFilter(newFilter);
+  };
 
-	const filteredTodos = todos.filter((todo) => {
-		if (filter === 'active') return !todo.isChecked;
-		if (filter === 'completed') return todo.isChecked;
-		return true;
-	});
+  const filteredTodos = todos.filter((todo) => {
+    if (filter === 'active') return !todo.isChecked;
+    if (filter === 'completed') return todo.isChecked;
+    return true;
+  });
 
-	return {
-		filter,
-		handleFilterChange,
-		filteredTodos,
-	};
+  return {
+    filter,
+    handleFilterChange,
+    filteredTodos,
+  };
 };
 
 export default useFilter;
--- src/hooks/useTodoFilter.ts
+++ src/hooks/useTodoFilter.ts
@@ -1,33 +1,33 @@
 import useTodos from './useTodos';
 import useFilter from './useFilter';
-import { Todo } from './types';
+import { Todo } from '../types/HookTypes';
 
 const useTodoFilter = (initialTodos: Todo[]) => {
-	const {
-		todos,
-		addTodo,
-		handleCheckChange,
-		handleClearCompleted,
-		toggleAllTodos,
-		deleteTodo,
-		checkedCount,
-		setTodos,
-	} = useTodos(initialTodos);
+  const {
+    todos,
+    addTodo,
+    handleCheckChange,
+    handleClearCompleted,
+    toggleAllTodos,
+    deleteTodo,
+    checkedCount,
+    setTodos,
+  } = useTodos(initialTodos);
 
-	const { filter, handleFilterChange, filteredTodos } = useFilter(todos);
+  const { filter, handleFilterChange, filteredTodos } = useFilter(todos);
 
-	return {
-		todos,
-		filteredTodos,
-		filter,
-		addTodo,
-		handleCheckChange,
-		handleFilterChange,
-		handleClearCompleted,
-		toggleAllTodos,
-		deleteTodo,
-		checkedCount,
-	};
+  return {
+    todos,
+    filteredTodos,
+    filter,
+    addTodo,
+    handleCheckChange,
+    handleFilterChange,
+    handleClearCompleted,
+    toggleAllTodos,
+    deleteTodo,
+    checkedCount,
+  };
 };
 
 export default useTodoFilter;
