5fecde5773675e30e7576a3480b5cf0be29c241b
dandamdandam <glue0440@gmail.com>
2024-06-09 10:40:54 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+const App = () => {
+    return(
+        <div>
+            dldl
+            {/* <Header />
+            <Input />
+            <TodoView /> */}
+        </div>
+    );
+};
+
+export default App;
