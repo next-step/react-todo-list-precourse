f7268afeade7964f72e7d911f522ae82b405d7d8
seongikx <tjddlr3512@pusan.ac.kr>
2024-06-09 20:08:04 +0900

    Feat: Todo 레이아웃 컴포넌트 구현

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoBox from "./components/TodoBox";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <TodoBox>안녕하세요</TodoBox>
+    </>
+  );
+}
+
+export default App;
