3158b512ff4ce70d3e1bd045a6bc3c99b6badcad
dandamdandam <glue0440@gmail.com>
2024-06-09 21:30:40 +0900

    refactor: split Home component
    todoList를 관리하는 함수와 UI 랜더링 컴포넌트로 나눔

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 
-import { Todo, ToggleTodoDone, AddTodo } from "./interfaces";
+import { Todo, ToggleTodoDone, AddTodo, RemoveTodo } from "./interfaces";
 import TodoView from "./components/TodoView";
 import InputTodo from "./components/InputTodo";
 
 /**
- * Todo 홈페이지 컴포넌트
- * + todoList state 관리
+ * todo state 관련된 CRUD 함수
+ * @param todoList 
+ * @param setTodoList 
+ * @returns 
  */
-const Home = () => {
-    const [todoList, setTodoList] = useState<Todo[]>([]);
+const TodoListFunc = (todoList: Todo[], setTodoList: React.Dispatch<React.SetStateAction<Todo[]>>) => {
     const toggleTodoDone: ToggleTodoDone = index => {
         todoList[index].done=!todoList[index].done;
         setTodoList([...todoList]);
@@ -19,10 +20,19 @@ const Home = () => {
         todoList.push({id: nextId, done: false, content: content});
         setTodoList([...todoList]);
     }
-    const removeTodo = (index: number) => {
+    const removeTodo: RemoveTodo = index => {
         todoList.splice(index, 1);
         setTodoList([...todoList]);
     }
+    return {toggleTodoDone, addTodo, removeTodo};
+}
+/**
+ * Todo 홈페이지 컴포넌트
+ * + todoList state 관리
+ */
+const Home = () => {
+    const [todoList, setTodoList] = useState<Todo[]>([]);
+    const {toggleTodoDone, addTodo, removeTodo} = TodoListFunc(todoList, setTodoList);
 
     return <div>
         {/* <Header /> */}
