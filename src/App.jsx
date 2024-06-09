8daeba4d4f2206727ca79f6894ef8031aa3ed4f9
joshuadesu <meron_pan@kakao.com>
2024-06-10 00:11:00 +0900

    Feat: Add TodoTemplate 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import "./Styles/App.css";
 import Header from "./Components/Header";
+import TodoTemplate from "./Components/TodoTemplate";
 import Footer from "./Components/Footer";
 
 const App = () => {
   return (
     <>
       <Header />
+      <TodoTemplate />
       <Footer />
     </>
   );
