f7580f1632311a1b9854773137014f6814f7a1a8
anheejeong <eyrt6973@naver.com>
2024-06-10 02:07:10 +0900

    Refacor: refactoring main file

--- src/main.jsx
+++ src/main.jsx
@@ -6,22 +6,14 @@ const Main = () => {
     const [newTodo, setNewTodo] = useState('');
     const [todoList, setTodoList] = useState([]);
     useEffect(() => {
-        console.log(newTodo);
         newTodo ? setTodoList([...todoList, newTodo]) : '';
         setNewTodo('');
     }, [newTodo])
-
     return (
         <div className="app">
             <h1>todos</h1>
-            <NewTodo
-                newText={newTodo}
-                setNewTodo={setNewTodo}
-            />
-            <TodoList
-                todoList={todoList}
-                setTodoList={setTodoList}
-            />
+            <NewTodo newText={newTodo} setNewTodo={setNewTodo} />
+            <TodoList todoList={todoList} setTodoList={setTodoList} />
         </div>
     );
 }
