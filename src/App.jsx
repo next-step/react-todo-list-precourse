c23cf8b3f7e5f183927afdb9bbb71cc8f5ebb462
dmsrud1218 <dmsrud566@naver.com>
2024-06-10 03:46:55 +0900

    feat: 모듈화를 위해 코드 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,16 @@
 import React, { useState } from "react";
 import "./App.css";
+import MainComponent from "./components/MainComponent.jsx";
 
 function App() {
-
-    const [todos, settodos] = useState([]);
-    const [newTask, setNewTask] = useState("");
-    
-    function handleInputChange(event){
-        setNewTask(event.target.value)
-    }
-    function addTask(){
-        if(newTask.trim() !==""){
-            settodos(t=> [...todos,newTask]);
-            setNewTask("");
-        } else {
-            alert("할 일을 입력해주세요!")
-        }
-    }
-
-    function deleteTask(index){
-        const updatedtodos = todos.filter((element, i)=> i !==index);
-        settodos(updatedtodos);
-    }
-    function handleKeyPress(event) {
-        if(event.key ==="Enter") {
-            addTask();
-        }
-    }
-   
-    return (
-        <div className="to-do-list">
-            <h1>To-Do-list</h1>
-
-            <div>
-                <input
-                    type="text"
-                    placeholder="할 일을 입력해보자..."
-                    value={newTask}
-                    onChange={handleInputChange}
-                    onKeyPress={handleKeyPress}/>
-                <button
-                    className="add-button" onClick={addTask}>
-                    Add
-                    </button>
-            </div>
-
-        <ol> 
-            {todos.map((task,index)=>   
-            <li key={index}>
-                <span className="text">{task}</span>
-                <button className="delete-button"
-                    onClick={() => deleteTask(index)}>
-                    Delete 
-                </button>
-
-            </li>
-        )}
-        </ol>
-        </div>
-    )
-};
-
+  const [todos, setTodos] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  return (
+    <div className="to-do-list">
+      <h1>To-Do-list</h1>
+      <MainComponent todos={todos}
+        setTodos={setTodos} newTask={newTask}
+        setNewTask={setNewTask} />
+    </div>);
+}
 export default App;
