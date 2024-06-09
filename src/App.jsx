7ba241c2c399f4864f7171a0ddaeb4d9c68531b4
joshuadesu <meron_pan@kakao.com>
2024-06-09 23:50:52 +0900

    Feat: Add Page HeaderとFooter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import "./Styles/App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 const App = () => {
   return (
     <>
-
+      <Header />
+      <Footer />
     </>
   );
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p>@2024 TodoList</p>
+      <p>Designed by Joshua from Los Angeles</p>
+    </div>
+  );
+}
+
+export default Footer;
