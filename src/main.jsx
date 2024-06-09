0e1125607fea919777bcf0dcacf34c110a7a625c
Hyoeunkh <lhe4528@knu.ac.kr>
2024-06-09 20:15:06 +0900

    feat: 초기 세팅 및 메인 화면 구현

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import Input from "./components/main/Input";
+import Todo from "./components/main/Todo";
+import Footer from "./components/main/Footer";
+import "./style/Global.css";
+
+export default function Main() {
+  const [todos, setTodos] = useState([]);
+  return (
+    <>
+      <div className="todo">
+        <h1>Hyo's Todo</h1>
+        <Input todos={todos} setTodos={setTodos} />
+        <Todo todos={todos} setTodos={setTodos} />
+      </div>
+      <Footer />
+    </>
+  );
+}
